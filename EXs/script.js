c8c34afb3aec486c3e7d9ece78c848cdb984b643
saugjong0521 <saugjong0521@gmail.com>
2024-08-19 19:12:48 +0900

    vault backup: 2024-08-19 19:12:47

--- EXs/script.js
+++ EXs/script.js
@@ -4,7 +4,12 @@ window.onload = () => {
 
 function menu (){
     /*
-
+    1. 메인메뉴를 over하면 선택된 메인메뉴의 서브메뉴만 드랍 다운
+    2. 서브메뉴의 길이에 따라서 header의 height가 달라짐
 
     */
+
+    const mainMenuItem = document.querySelectorAll('.main-menu li a')
+
+
 }
