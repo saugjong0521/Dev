0b11bd96fe5906b62ae73b04548c2cf59cada435
saugjong0521 <saugjong0521@gmail.com>
2024-08-19 20:38:50 +0900

    vault backup: 2024-08-19 20:38:49

--- EXs/script.js
+++ EXs/script.js
@@ -40,7 +40,8 @@ function menu (){
     })
     // mouseover, mouseout 으로 지정시에 하위 li로 간다면 focus가 풀림
 
-    subMenuItem.forEach((el)=>{
+    subMenu.forEach((el)=>{
+        const subMenuItem = el.querySelector('a')
         subMenuItem.classList.add('on')
 
     })
