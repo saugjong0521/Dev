2683bce5acc1463a866c8c8681798266bef089be
saugjong0521 <saugjong0521@gmail.com>
2024-08-19 19:16:48 +0900

    vault backup: 2024-08-19 19:16:47

--- EXs/script.js
+++ EXs/script.js
@@ -8,8 +8,20 @@ function menu (){
     2. 서브메뉴의 길이에 따라서 header의 height가 달라짐
 
     */
+{}
+    const mainMenu = document.querySelectorAll('main-menu li')
 
-    const mainMenuItem = document.querySelectorAll('.main-menu li a')
+    mainMenu.forEach((el)=>{
+        const mainMenuLink = el.querySelector('a');
+        //각각의 mainmenu에 있는 a태그를 선택
+        mainMenuLink.addEventListener('mouseover', onMenu)
+    })
+
+    function onMenu(){
+        this.classList.add('.on')
+    }
+
+    // const mainMenuItem = document.querySelectorAll('.main-menu li a')
 
 
 }
