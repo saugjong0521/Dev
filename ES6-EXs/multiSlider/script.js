34fd788e2f307a315a0ea277ecf037f649db058a
saugjong0521 <149376211+saugjong0521@users.noreply.github.com>
2024-08-21 14:49:19 +0900

    script.js

--- ES6-EXs/multiSlider/script.js
+++ ES6-EXs/multiSlider/script.js
@@ -1,53 +1,70 @@
 window.onload = () => {
-
-
     const bgSlider = document.querySelector('#slider')
-    const slider = document.querySelector('#slider2')
-
+    const mSlider = document.querySelector('#slider2')
     const next = document.querySelector('.next')
     const prev = document.querySelector('.prev')
 
     const speed = 1000;
 
-    phoneSlider();
-    function phoneSlider() {
-        const itemUl = slider.querySelector('ul')
-        const itemLi = itemUl.querySelectorAll('li')
-        const itemSize = itemLi.length;
-        console.log(itemSize)
+    mobileSlider()
+    function mobileSlider() {
+        const itemUl = document.querySelector('#slider2 ul')
+        const itemLi = document.querySelectorAll('#slider2 ul li')
+        const itemCount = itemLi.length;
+        console.log(itemCount)
 
+        itemUl.style.width = `${100 * itemCount}%`;
+        itemUl.style.height = '100%';
+        itemUl.style.marginLeft = `-100%`;
+        console.log(itemUl.style.height)
 
-        // ul 설정
-        itemUl.style.width = `${100 * itemSize}%`;
-        itemUl.style.height = `100%`;
-        itemUl.style.marginLeft = `-100%`
-
-        // li 설정
         itemLi.forEach((el) => {
-            el.style.width = `${100 / itemSize}%`
-            el.style.height = `100%`
+            el.style.width = `${100 / itemCount}%`;
+            el.style.height = `100%`;
             el.style.float = 'left'
+            itemUl.insertBefore(itemLi[itemCount - 1], itemUl.firstChild)
         })
-        itemUl.insertBefore(itemLi[itemSize - 1], itemUl.firstChild)
     }
 
-    next.addEventListener('click', function () {
-        nextSlider();
+    next.addEventListener('click',function(){
+        nextSlide()
     })
 
-    function nextSlider() {
-        const itemUl = slider.querySelector('ul')
-        const itemLiW = itemUl.querySelector('li').offsetWidth;
+    function nextSlide(){
+        const itemUl = slider2.querySelector('#slider2 ul')
+        const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
         console.log(itemLiW)
 
+        console.log(itemUl)
+
+        itemUl.style.marginLeft = `-${itemLiW * 2}`
         itemUl.style.transition = `margin-left ${speed}ms`;
-        itemLiW.style.marginLeft = `-${itemLiW * 2}px`
 
         setTimeout(()=>{
-            itemUl.appendChild(itemUl.querySelector(`li:first-of-child`))
+            itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
             itemUl.style.marginLeft = `-${itemLiW}px`
             itemUl.transition = '';
         },speed)
+        
     }
+
+    prev.addEventListener('click',function(){
+        const itemUl = slider2.querySelector('#slider2 ul')
+        const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
+        console.log(itemLiW)
+
+        console.log(itemUl)
+
+        itemUl.style.marginLeft = `${itemLiW * 2}`
+        itemUl.style.transition = `margin-left ${speed}ms`;
+
+        setTimeout(()=>{
+            itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
+            itemUl.style.marginLeft = `-${itemLiW}px`
+            itemUl.transition = '';
+        },speed)
+    })
+
+
 
 }
