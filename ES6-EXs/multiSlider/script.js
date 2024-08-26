6e766fec17a930ea975c81c8af649cdea205d16b
saugjong0521 <saugjong0521@gmail.com>
2024-08-26 21:29:08 +0900

    vault backup: 2024-08-26 21:29:08

--- ES6-EXs/multiSlider/script.js
+++ ES6-EXs/multiSlider/script.js
@@ -30,51 +30,52 @@ window.onload = () => {
             el.style.height = `100%`;
             el.style.float = 'left';
 
+        })
+
+        next.addEventListener('click', function () {
+            nextSlide()
+        })
+
+        function nextSlide() {
+            const itemUl = document.querySelector('#slider2 ul')
+            const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
+            console.log(itemLiW)
+
+            console.log(itemUl)
+
+            itemUl.style.marginLeft = `-${itemLiW * 2}px`
+            itemUl.style.transition = `margin-left ${speed}ms`;
+
+            setTimeout(() => {
+                itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
+                itemUl.style.marginLeft = `-${itemLiW}px`
+                itemUl.transition = '';
+            }, speed)
+
+        }
+
+        prev.addEventListener('click', function () {
+            prevSlide()
+        })
+
+        function prevSlide() {
+            const itemUl = document.querySelector('#slider2 ul')
+            const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
+            console.log(itemLiW)
+
+            console.log(itemUl)
+
+            itemUl.style.marginLeft = `${itemLiW / 2}px`
+            itemUl.style.transition = `margin-left ${speed}ms`;
+
+            setTimeout(() => {
+                itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
+                itemUl.style.marginLeft = `-${itemLiW}px`
+                itemUl.transition = '';
+            }, speed)
+        }
+
+
+
     }
-
-    next.addEventListener('click', function () {
-        nextSlide()
-    })
-
-    function nextSlide() {
-        const itemUl = document.querySelector('#slider2 ul')
-        const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
-        console.log(itemLiW)
-
-        console.log(itemUl)
-
-        itemUl.style.marginLeft = `-${itemLiW * 2}px`
-        itemUl.style.transition = `margin-left ${speed}ms`;
-
-        setTimeout(() => {
-            itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
-            itemUl.style.marginLeft = `-${itemLiW}px`
-            itemUl.transition = '';
-        }, speed)
-
-    }
-
-    prev.addEventListener('click', function () {
-        prevSlide()
-    })
-
-    function prevSlide() {
-        const itemUl = document.querySelector('#slider2 ul')
-        const itemLiW = document.querySelector('#slider2 ul li').offsetWidth;
-        console.log(itemLiW)
-
-        console.log(itemUl)
-
-        itemUl.style.marginLeft = `${itemLiW/2}px`
-        itemUl.style.transition = `margin-left ${speed}ms`;
-
-        setTimeout(() => {
-            itemUl.appendChild(itemUl.querySelector(`li:first-of-type`))
-            itemUl.style.marginLeft = `-${itemLiW}px`
-            itemUl.transition = '';
-        }, speed)
-    }
-
-
-
 }
