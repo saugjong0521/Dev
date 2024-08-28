6fb3b9bff025120ee9e9f396476f68df7d535fc0
saugjong0521 <saugjong0521@gmail.com>
2024-08-28 19:26:27 +0900

    vault backup: 2024-08-28 19:26:27

--- ES6-EXs/pallarex/script02.js
+++ ES6-EXs/pallarex/script02.js
@@ -22,4 +22,43 @@ function parallaxPage() {
         console.log(sectionOffset)
     }
 
+    function bindingEvent() {
+        window.addEventListener('resize', calcSectionOffset)
+        section.forEach(el => el.addEventListener('wheel', onwheel))
+    }
+
+    function onWheel(e) {
+        const deltaY = e.deltaY || -e.wheelDelta || e.detail;
+        const currentIdx = Array.from(section).indexOf(this);
+
+        if (deltaY < 0 && currentIdx > 0) {
+            console.log(currentIdx - 1)
+            moveSection(currentIdx - 1)
+        } else if (deltaY > 0 && currentIdx < 0 && section.length - 1) {
+            console.log(currentIdx + 1)
+            moveSection(currentIdx + 1)
+        }
+    }
+
+    function moveSection(idx) {
+        const targetOffset = sectionOffset[idx];
+        const currentScrollP = decument.documentElement.scrollTop;
+        const scrollDisP = targetOffset - currentScrollP;
+        const speed = 500;
+        let isPlay;
+
+        function scrollMotion(step) {
+            if (!isPlay) { 
+                isPlay = step;
+            }
+            const progress = step - isPlay;
+            const percent = Math.min(progress / speed, 1)
+            window.scrollTo(0, currentScrollP + scrollDisP * percent);
+            if(progress < speed){
+                window.requestAnimationFrame(scrollMotion);
+            }
+        }
+        window.requestAnimationFrame(scrollMotion);
+    }
+
 }
