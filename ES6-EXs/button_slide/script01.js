b0a60e24fa7434ed40f7fa897be6d901424f8f5d
saugjong0521 <saugjong0521@gmail.com>
2024-08-28 20:42:23 +0900

    vault backup: 2024-08-28 20:42:22

--- ES6-EXs/button_slide/script01.js
+++ ES6-EXs/button_slide/script01.js
@@ -10,6 +10,7 @@ function slider() {
     const btnItem = document.querySelectorAll('.navi > li');
 
     let speed = 500;
+    let timer;
 
     const circle = document.querySelector('#clrcle');
 
@@ -59,16 +60,27 @@ function slider() {
         console.log(startActive)
         let currentVal; // 현재 애니메이션이 적용되는 객체의 속성을 전달해줄 변수
 
-        if(opt.prop == 'opactiy'){
+        if (opt.prop == 'opactiy') {
             currentVal = parseFloat(getComputedStyle(el)[opt.prop])
             console.log(currentVal)
-        } else{
+        } else {
             currentVal = parseFloat(getComputedStyle(el)[opt.prop])
             console.log(currentVal)
         }
 
-        if(currentVal !== opt.val){
+        if (currentVal !== opt.val) {
             requestAnimationFrame(slide)
+        }
+
+        function slide(time) {
+            let lastTime = time - startActive;
+            let currentTime = lastTime / opt.duration;
+
+            if(currentTime < 1){
+                timer = requestAnimationFrame(slide)
+            } else {
+                cancelAnimationFrame(slide)
+            }
         }
 
     }
