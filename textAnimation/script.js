54e5eb4b6fe6cbc54eb1114e1878c23135d88cc1
saugjong0521 <saugjong0521@gmail.com>
2024-08-30 21:25:26 +0900

    vault backup: 2024-08-30 21:25:25

--- textAnimation/script.js
+++ textAnimation/script.js
@@ -7,9 +7,14 @@ function textAnimation() {
     const item = document.querySelectorAll('.word-box');
     let currentIndex = 0;
 
-    currentIndex = (currentIndex + 1) % item.length;
-    item[currentIndex].classList.remove('out');
-    item[currentIndex].style.opacity = 0;
+    function toggleAnimation() {
+        currentIndex = (currentIndex + 1) % item.length;
+        item[currentIndex].classList.remove('out');
+        item[currentIndex].style.opacity = 0;
 
-    item[currentIndex].classList.add('in')
+        item[currentIndex].classList.add('in');
+        item[currentIndex].style.opacity = 1;
+    }
+
+    setInterval(toggleAnimation, 1500)
 }
