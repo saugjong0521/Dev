7788d9cb5a35290f63590a32164e82bca1d9a29e
saugjong0521 <saugjong0521@gmail.com>
2024-08-30 21:23:30 +0900

    vault backup: 2024-08-30 21:23:29

--- textAnimation/script.js
+++ textAnimation/script.js
@@ -8,4 +8,8 @@ function textAnimation() {
     let currentIndex = 0;
 
     currentIndex = (currentIndex + 1) % item.length;
+    item[currentIndex].classList.remove('out');
+    item[currentIndex].style.opacity = 0;
+
+    item[currentIndex].classList.add('in')
 }
