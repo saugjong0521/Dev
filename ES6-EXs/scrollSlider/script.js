7d0999bb985d20ab7bfc1d0eeeb6b3930464f3e8
saugjong0521 <saugjong0521@gmail.com>
2024-09-02 19:11:06 +0900

    vault backup: 2024-09-02 19:11:06

---
+++ ES6-EXs/scrollSlider/script.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+    /*
+    가로 스크롤의 값을 세로 스크롤 값에 추가해서 전체 스크롤의 길이를 늘리는 구조
+
+    */
+
+    const wScrollTop = window.pageYOffset;  // 현재 나의 스크롤 위치
+    //const 
+    console.log(wScrollTop)
+    
+}
