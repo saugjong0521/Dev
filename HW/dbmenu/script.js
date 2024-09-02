39ac325412962e22750551f2348291ccda0648cb
saugjong0521 <saugjong0521@gmail.com>
2024-09-02 09:38:24 +0900

    vault backup: 2024-09-02 09:38:24

--- HW/dbmenu/script.js
+++ HW/dbmenu/script.js
@@ -27,12 +27,7 @@ function submenu() {
 
     function onOver() {
         let menuList = document.querySelector('.gnb .submenu')
-
-        menuList.forEach((el, idx) => {
-            el[idx].classList.add('on')
-        })
-
-        /let idx = Array.from(menuList).indexOf(this)
+        let idx = Array.from(menuList).indexOf(this)
 
         menuList.classList.add('on')
 
