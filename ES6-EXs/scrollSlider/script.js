7115fd9542af96fe479d4af91e21c1f3d4214733
saugjong0521 <saugjong0521@gmail.com>
2024-09-02 19:58:04 +0900

    vault backup: 2024-09-02 19:58:04

--- ES6-EXs/scrollSlider/script.js
+++ ES6-EXs/scrollSlider/script.js
@@ -33,7 +33,7 @@ window.onload = () => {
 
     function setActive(el) {
         const contentY = el.getBoundingClientRect()
-        // 각 요소의 크기와 위치를 받아옴 (현재 화면에 보이는지 체크하기 위함)
+        // getBoundingClientRect: 각 요소의 크기와 위치를 받아옴 (현재 화면에 보이는지 체크하기 위함)
         console.log(contentY)
         el.querySelectorAll(`.${itemClass}`).forEach((itemClassEl) => {
             // 특정 요소에 있는 클래스를 제거
