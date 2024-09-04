9bbf02dd74c91428d4a73dcbae6e46f4822e0125
saugjong0521 <saugjong0521@gmail.com>
2024-09-04 19:10:56 +0900

    vault backup: 2024-09-04 19:10:56

--- ES6-EXs/09.date/script.js
+++ ES6-EXs/09.date/script.js
@@ -4,6 +4,10 @@ window.onload = () => {
 
 /*
     select를 찾아서 안의 option들의 값을 찾아서 내용을 새로 생성되는 ul > li 구조의 형태에 그대로 이식하는 것
+
+    1. select에 있는 option의 갯수 파악
+    2. option안에 있는 값을 출력
+    3. 1,2 에 있는 새로 생성되는 태그에 이식
 */
 
 function formData(){
