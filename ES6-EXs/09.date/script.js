7f1bc38e94d993942e4d2ee8ebdc8781dc8bfb7e
saugjong0521 <saugjong0521@gmail.com>
2024-09-04 19:08:50 +0900

    vault backup: 2024-09-04 19:08:50

---
+++ ES6-EXs/09.date/script.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    formData();
+}
+
+/*
+    select를 찾아서 안의 option들의 값을 찾아서 내용을 새로 생성되는 ul > li 구조의 형태에 그대로 이식하는 것
+*/
+
+function formData(){
+    const selects = document.querySelectorAll('select')
+    console.log(selects)
+
+    selects.forEach(el => {
+
+    })
+}
