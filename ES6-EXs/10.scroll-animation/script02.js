948d275cdf2f0f915c273b513c75b3fc335e08c8
saugjong0521 <saugjong0521@gmail.com>
2024-09-05 17:45:19 +0900

    vault backup: 2024-09-05 17:45:18

---
+++ ES6-EXs/10.scroll-animation/script02.js
@@ -0,0 +1,10 @@
+class ScrollAction {
+    constructor(){
+        currentPosition()  //현재 위치를 전달
+        checkPosition() //횡스크롤 위치를 찾음
+        activeScroll()  //횡스크롤 시작
+        endScroll() //횡스크롤 종료
+    }ytgf
+
+    
+}
