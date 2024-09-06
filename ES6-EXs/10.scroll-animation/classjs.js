058035bd8631237cc728d385275ffee6fe522d8e
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 20:07:47 +0900

    vault backup: 2024-09-06 20:07:47

--- ES6-EXs/10.scroll-animation/classjs.js
+++ ES6-EXs/10.scroll-animation/classjs.js
@@ -1,7 +1,12 @@
 class ScrollAnimation {
-    constructor(option = {}){
+    constructor(option = {}) {
         this.opt = object.assign({
-            
-        })
+            canvasWrap: this.canvasWrap,
+            scrollBody: this.scrollBody,
+            imgSrc: this.imgSrc,
+            imgFormat: this.imgFormat,
+            imgLength: this.imgLength
+            // 객체를 만들어 두고 이후 window.onload를 통하여 어떤 값인지를 넣어줌
+        }, option)
     }
 }
