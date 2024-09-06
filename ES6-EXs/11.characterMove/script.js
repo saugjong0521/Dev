5892618834c83dada11b49551bc7cc4f6b289722
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 21:21:50 +0900

    vault backup: 2024-09-06 21:21:50

--- ES6-EXs/11.characterMove/script.js
+++ ES6-EXs/11.characterMove/script.js
@@ -2,8 +2,26 @@ window.onload = () => {
 
 
     window.addEventListener('mousemove', (e) => {
-        const img = document.querySelector('pic > li.om img');
+        const img = document.querySelector('pic li.on img');
         const deco2 = document.querySelector('deco2 img');
         const deco3 = document.querySelector('deco3 img');
+
+        moveImg(img, e, 30, true);  //넘겨줄 애들
     })
+
+    function moveImg(el, e, speed, reverse) {
+        let x;
+        let y;
+        if (reversex) {
+            x = -e.pageX;
+            y = -e.pageY
+        } else {
+            x = e.pageX;
+            y = e.pageY
+        }
+
+        el.style.left = x / speed + 'px';
+        el.style.top = y / speed + 'px';
+    }
+
 }
