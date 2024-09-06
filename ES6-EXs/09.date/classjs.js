c9fe74cf7ce17218744013007ce3fc2bfa4f4650
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 19:24:49 +0900

    vault backup: 2024-09-06 19:24:49

--- ES6-EXs/09.date/classjs.js
+++ ES6-EXs/09.date/classjs.js
@@ -18,7 +18,12 @@ class FormData {
             const wrapper = document.createElement('div')
             wrapper.className = 'select';
             el.parentNode.insertBefore(wrapper, el);
-            wrapper.appendChild(el)
+            wrapper.appendChild(el);
+
+            const styledSelect = document.createElement('div')
+            styledSelect.className = 'select-styled';
+            styledSelect.textContent = el.children[0].textContent;
+            wrapper.appendChild(styledSelect)
         })
     }
 }
