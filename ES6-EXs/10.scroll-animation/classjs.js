662275bb4253df1f424b0e0fe03b31dc11437463
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 20:45:47 +0900

    vault backup: 2024-09-06 20:45:47

--- ES6-EXs/10.scroll-animation/classjs.js
+++ ES6-EXs/10.scroll-animation/classjs.js
@@ -1,7 +1,7 @@
 class ScrollAnimation {
     constructor(option = {}) {
         // 위의 option은 생성자에 전달되는 매개변수
-        this.opt = object.assign({
+        this.opt = Object.assign({
             canvasWrap: this.canvasWrap,
             scrollBody: this.scrollBody,
             imgSrc: this.imgSrc,
@@ -40,9 +40,49 @@ class ScrollAnimation {
     }
 
     init() {
+        // 아래의 경우, 이미지가 한장만 로드되는 것이 아닌, 여러장을 묶어서 로드해야 하기 때문에, 초기값으로 이미지를 넣고, 이후 init에서 함수를 통해 분리해서 지연시간을 가지게 한다.
         this.el.appendChild(this.canvas);
-        for (let i = 0; i < this.imgLength; i++) {
+        for(let i =0; i < this.imgLength; i++){
+            const img = new Image();
+            const imgPath = `${this.imgSrc}${i}${this.imgFormat}`;
+            img.src = imgPath;
 
+            img.onload = () =>{
+                this.currentImg += 1;
+                if(this.currentImg === this.imgLength){
+                    this.setProperty();
+                    this.scrollFunc();
+                }
+            }
+            this.imgArr.push(img)
         }
+    }
+    setProperty(){
+        this.scrollHeight = this.scrollBody.offsetHeight;
+        this.winScrollTop = window.pageYOffset;
+        this.sectionOffsetTop = this.scrollBody.getBoundingClientRect().top + this.winScrollTop;
+        this.scrollRealHeight = this.scrollHeight - window.innerHeight;
+        this.sectionScrollTop = this.winScrollTop - this.sectionOffsetTop;
+        this.scrollPercent = this.sectionScrollTop / this.scrollRealHeight;
+        this.percent = this.scrollPercent * 100;
+
+        //스크롤 높이가 0이하일때 처리
+        if(this.scrollRealHeight <= 0){
+            this.scrollPercent = 0;
+        }
+
+        this.canvas.width = this.imgWidth;
+        this.canvas.height = this.imgHeight
+    }
+
+    scrollFunc(){
+        const sequence = Math.min(this.imgLength - 1, Math.max(0, Math.floor(this.imgLength * this.scrollPercent)))
+        if(this.imgArr[sequence]){
+            this.renderCanvas(sequence);
+        }
+    }
+    renderCanvas(sequence){
+        this.ctx.clearRect(0,0,this.imgWidth, this.imgHeight);
+        this.ctx.drawImage(this.imgArr[sequence],0,0,this.imgWidth,this.imgHeight);
     }
 }
