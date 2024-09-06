1648835c868a45163b2b1d3be04c5b618c369ee2
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 20:08:48 +0900

    vault backup: 2024-09-06 20:08:48

--- ES6-EXs/10.scroll-animation/classjs.js
+++ ES6-EXs/10.scroll-animation/classjs.js
@@ -1,5 +1,6 @@
 class ScrollAnimation {
     constructor(option = {}) {
+        // 위의 option은 생성자에 전달되는 매개변수
         this.opt = object.assign({
             canvasWrap: this.canvasWrap,
             scrollBody: this.scrollBody,
@@ -8,5 +9,6 @@ class ScrollAnimation {
             imgLength: this.imgLength
             // 객체를 만들어 두고 이후 window.onload를 통하여 어떤 값인지를 넣어줌
         }, option)
+        // 여기의 option은 다른 함수에 전달될 매개변수
     }
 }
