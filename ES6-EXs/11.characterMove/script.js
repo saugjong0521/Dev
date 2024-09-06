17522f1d4a3dba5237c07519ddea64959af19d17
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 21:23:48 +0900

    vault backup: 2024-09-06 21:23:47

--- ES6-EXs/11.characterMove/script.js
+++ ES6-EXs/11.characterMove/script.js
@@ -2,18 +2,20 @@ window.onload = () => {
 
 
     window.addEventListener('mousemove', (e) => {
-        const img = document.querySelector('pic li.on img');
-        const deco2 = document.querySelector('deco2 img');
-        const deco3 = document.querySelector('deco3 img');
+        const img = document.querySelector('.pic li.on img');
+        const deco2 = document.querySelector('.deco2 img');
+        const deco3 = document.querySelector('.deco3 img');
 
-        moveImg(img, e, 30, true);  //넘겨줄 애들
+        moveImg(img, e, 30, true);  //괄호 안의 내용은 넘겨줄 애들
         moveImg(deco2, e, 100, false)
+        moveImg(deco3, e, 100, false)
+
     })
 
     function moveImg(el, e, speed, reverse) {
         let x;
         let y;
-        if (reversex) {
+        if (reverse) {
             x = -e.pageX;
             y = -e.pageY
         } else {
