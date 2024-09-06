43536b8eab8539619b14cac8830ea2691c9384f7
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 19:31:46 +0900

    vault backup: 2024-09-06 19:31:46

--- ES6-EXs/09.date/classjs.js
+++ ES6-EXs/09.date/classjs.js
@@ -6,11 +6,18 @@ window.onload = () => {
 
 class FormData {
     constructor() {
-        this.selects = document.querySelectorAll('select')
+        
         this.init();
+        this.bindingEvent();
     }
 
     init() {
+        /*
+        init는 초기값을 의미하는 말로, 선택자; 시작값; 등을 작성
+
+        다만 안에서 선언 시, 지역변수가 되기에
+        밖에서 선언 이후 값을 init에다가 값을 작성
+        */
         this.selects.forEach(el => {
             const selectItem = el.children.length;
 
@@ -23,7 +30,18 @@ class FormData {
             const styledSelect = document.createElement('div')
             styledSelect.className = 'select-styled';
             styledSelect.textContent = el.children[0].textContent;
-            wrapper.appendChild(styledSelect)
+            wrapper.appendChild(styledSelect);
+
+            const list = document.createElement('ul');
+            list.className = 'select-options';
+            wrapper.appendChild(list);
+
+            for(let i = 0; i < selectItem; i++){
+                const listItem = document.createElement('li');
+                listItem.textContent = el.children[i].textContent;
+                listItem.setAttribute('rel', el.childre[i].value)
+                list.appendChild()
+            }
         })
     }
 }
