b2899edada5abd15db9dc4dfa2c72f44aa4e077b
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 20:51:50 +0900

    vault backup: 2024-09-06 20:51:49

--- ES6-EXs/10.scroll-animation/script.js
+++ ES6-EXs/10.scroll-animation/script.js
@@ -1,4 +1,4 @@
-    window.onload = () => {
+window.onload = () => {
     /*
     스크롤이 되는 값을 받아와서, 특정 위치에서 이미지를 교체해서 애니메이션처럼 보이게 하기
 
@@ -67,7 +67,7 @@
         sectionTop = scrollW - sectionOffsetTop;
         scrollPercent = sectionTop / sectionRealH;
         percent = scrollPercent * 100;
-        
+
         // console.log(sectionTop)
 
         // console.log(sectionRealH)
@@ -88,7 +88,7 @@
     }
 
     function scrollFunc() {
-        const sequence = Math.min(imgLength - 1 ,Math.floor(scrollPercent * imgLength))
+        const sequence = Math.min(imgLength - 1, Math.floor(scrollPercent * imgLength))
         if (imgArr[sequence]) {
             canvasRender(sequence)
         }
@@ -104,19 +104,19 @@
         ctx.drawImage(imgArr[sequence], 0, 0, imgWidth, imgHeight)
     }
 
-    function contentIn () {
-        
-        if(percent >= 40 && percent < 45){
+    function contentIn() {
+
+        if (percent >= 40 && percent < 45) {
             document.querySelector('.pos1').classList.add('active')
         }
-        if(percent >= 45 && percent < 50){
+        if (percent >= 45 && percent < 50) {
             document.querySelector('.pos2').classList.add('active')
         }
-        if(percent>= 50){
+        if (percent >= 50) {
             document.querySelector('.pos3').classList.add('active')
         }
 
-        if(percent > 65 || percent < 39){
+        if (percent > 65 || percent < 39) {
             document.querySelector('.pos1').classList.remove('active')
             document.querySelector('.pos2').classList.remove('active')
             document.querySelector('.pos3').classList.remove('active')
@@ -124,7 +124,7 @@
         console.log(percent)
     }
 
-    window.addEventListener('scroll',()=> {
+    window.addEventListener('scroll', () => {
         setProperty();
         scrollFunc();
     })
