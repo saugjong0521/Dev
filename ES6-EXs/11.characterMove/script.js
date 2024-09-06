72a0ff621f92ca4bfba09b164e97ab8273df96b7
saugjong0521 <saugjong0521@gmail.com>
2024-09-06 21:17:47 +0900

    vault backup: 2024-09-06 21:17:47

---
+++ ES6-EXs/11.characterMove/script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+
+
+    window.addEventListener('mousemove', (e) => {
+        const img = document.querySelector('pic > li.om img');
+        const deco2 = document.querySelector('deco2 img');
+        const deco3 = document.querySelector('deco3 img');
+    })
+}
