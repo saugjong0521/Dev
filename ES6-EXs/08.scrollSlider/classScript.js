95eeb09c3bae3b3858229209d610913ec2bb1e63
saugjong0521 <saugjong0521@gmail.com>
2024-09-07 09:00:39 +0900

    vault backup: 2024-09-07 09:00:39

--- ES6-EXs/08.scrollSlider/classScript.js
+++ ES6-EXs/08.scrollSlider/classScript.js
@@ -1,25 +1,5 @@
 class ScrollAction {
-    constructor(el) {
-        init(el)  
-        checkPosition() //현재 위치를 찾고 전달
-        activeScroll()  //횡스크롤 시작
-        endScroll() //횡스크롤 종료
-    }
-
-    init(el) {
-        this.currentScroll = window.pageYOffset
-        this.winH = window.innerHeight
-        this.winW = window.innerWidth
-
-        this.itemActive = `slider-active`
-        this.itemEnd = `slider-end`
-
-        this.sliderSection = document.querySelectorAll(el)
-    }
-    
-    checkPosition () {
-
+    constructor() {
         
-    }
 
-}
+    }
