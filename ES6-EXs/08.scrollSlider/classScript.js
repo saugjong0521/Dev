bc2b88b9ecfd4b4f37f3ea47ffac0dea7bc1ffb7
saugjong0521 <saugjong0521@gmail.com>
2024-09-07 09:33:41 +0900

    vault backup: 2024-09-07 09:33:41

--- ES6-EXs/08.scrollSlider/classScript.js
+++ ES6-EXs/08.scrollSlider/classScript.js
@@ -1,59 +1,61 @@
 window.onload = () => {
-    class ScrollAction {
-        constructor() {
 
-            this.currentScrollH = window.pageYOffset;
-            this.winH = window.innerHeight;
-            this.winW = window.innerWidth;
+    const scrollAction = new ScrollAction();
+}
+class ScrollAction {
+    constructor() {
 
-            // Object.assign으로 가능
-            this.itemClass.className = `slider`;
-            this.itemActive.className = `slider-active`;
-            this.itemEnd.className = `slider-end`;
-            this.horizonSection = document.querySelectorAll('.horizontal-scroll')
+        this.currentScrollH = window.pageYOffset;
+        this.winH = window.innerHeight;
+        this.winW = window.innerWidth;
 
-            console.log(itemClass)
+        // Object.assign으로 가능
+        this.itemClass.className = `slider`;
+        this.itemActive.className = `slider-active`;
+        this.itemEnd.className = `slider-end`;
+        this.horizonSection = document.querySelectorAll('.horizontal-scroll')
 
-            bindingEvent();
+        console.log(itemClass)
 
-
-
-            /*
-            1. bindingEvent = scroll시 일어날 함수들 입력
-            2. 현재 위치를 읽어올 함수 -> el로 위치값 전달
-            3. 특정 위치(horizonSection에 도달하면) slider-active적용
-            4. 특정 위치가 끝나면 slider-end적용
-            5. 해당 위치에
-    
-            */
-
-
-        }
-
-        bindingEvent() {
-            window.addEventListener('scroll', () => {
-                this.horizonSection.forEach(el, () => {
-                    activeScroll(el)
-
-                })
-            })
-        }
-
-        activeScroll(el) {
-            this.contentY = el.getBoundingClientRect();
-
-            el.querySelectorAll(this.itemClass).forEach(e => {
-                e.classList.remove(this.itemActive);
-                e.classList.remove(this.itemEnd);
-            })
-
-            if (contentY < 0) {
-                el.classList.add(this.itemEnd)
-            }
+        bindingEvent();
 
 
 
-        }
+        /*
+        1. bindingEvent = scroll시 일어날 함수들 입력
+        2. 현재 위치를 읽어올 함수 -> el로 위치값 전달
+        3. 특정 위치(horizonSection에 도달하면) slider-active적용
+        4. 특정 위치가 끝나면 slider-end적용
+        5. 해당 위치에
+
+        */
+
 
     }
+
+    bindingEvent() {
+        window.addEventListener('scroll', () => {
+            this.horizonSection.forEach(el, () => {
+                activeScroll(el)
+
+            })
+        })
+    }
+
+    activeScroll(el) {
+        this.contentY = el.getBoundingClientRect();
+
+        el.querySelectorAll(this.itemClass).forEach(e => {
+            e.classList.remove(this.itemActive);
+            e.classList.remove(this.itemEnd);
+        })
+
+        if (contentY < 0) {
+            el.classList.add(this.itemEnd)
+        }
+
+
+
+    }
+
 }
