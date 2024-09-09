cdc2adefa38cc4bb74cdde731004405b07819796
saugjong0521 <saugjong0521@gmail.com>
2024-09-09 20:13:20 +0900

    vault backup: 2024-09-09 20:13:20

--- ES6-EXs/12.apple-interaction/script/script.js
+++ ES6-EXs/12.apple-interaction/script/script.js
@@ -1,3 +1,9 @@
 window.onload = () => {
 
+    let yOffset = 0; // window.pageYoffset을 대입할 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전 스크롤 섹션의 값
+    let currentSection = 0; // 현재 활성화된 섹션
+    let newSection = false; // 새로운 섹션이 시작됨을 알려줌
+
+
 }
