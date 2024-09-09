42ba6ec7a1b77bc259b2209aeea156552c789c17
saugjong0521 <saugjong0521@gmail.com>
2024-09-09 20:16:21 +0900

    vault backup: 2024-09-09 20:16:21

--- ES6-EXs/12.apple-interaction/script/script.js
+++ ES6-EXs/12.apple-interaction/script/script.js
@@ -4,6 +4,19 @@ window.onload = () => {
     let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전 스크롤 섹션의 값
     let currentSection = 0; // 현재 활성화된 섹션
     let newSection = false; // 새로운 섹션이 시작됨을 알려줌
+    let acc = 0.2;
+    let delayYoffset = 0;
+    let rafId;
+    let rafState;
+
+
+    const sectionInfo = [
+        {
+            type: 'sticky', // 화면에 보여질 방식
+            heightNum: 5,   // 해당 섹션의 높이를 브라우저의 높이의 5배로 설정함
+
+        }
+    ]
 
 
 }
