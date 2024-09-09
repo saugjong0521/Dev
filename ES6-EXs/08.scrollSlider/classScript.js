b706e5a444db906c752d6f4b2214d546af539a6a
saugjong0521 <saugjong0521@gmail.com>
2024-09-09 14:54:02 +0900

    vault backup: 2024-09-09 14:54:04

--- ES6-EXs/08.scrollSlider/classScript.js
+++ ES6-EXs/08.scrollSlider/classScript.js
@@ -19,7 +19,6 @@ class ScrollAction {
         this.bindingEvent();
 
 
-
         /*
         1. bindingEvent = scroll시 일어날 함수들 입력
         2. 현재 위치를 읽어올 함수 -> el로 위치값 전달
@@ -35,32 +34,79 @@ class ScrollAction {
     bindingEvent() {
 
         this.horizonSection.forEach(el => {
-            setScroll();
-            setActive();
+            this.setScroll(el);
+            this.setActive(el);
         })
 
         window.addEventListener('scroll', () => {
-            this.horizonSection.forEach(el, () => {
-                activeScroll(el)
+            this.currentScrollH = window.pageYOffset;
+            this.horizonSection.forEach(el => {
+                this.setActive(el)
+                this.activeScroll(el)
 
             })
         })
     }
 
-    activeScroll(el) {
-        this.contentY = el.getBoundingClientRect();
+    setActive(el) {
+        const contentY = el.getBoundingClientRect();
+                // console.log(contentY)
 
-        el.querySelectorAll(this.itemClass).forEach(e => {
+        el.querySelectorAll(`.${this.itemClass}`).forEach(e => {
             e.classList.remove(this.itemActive);
             e.classList.remove(this.itemEnd);
         })
 
-        if (contentY < 0) {
-            el.classList.add(this.itemEnd)
+        if (contentY.bottom < 0) {
+            el.querySelectorAll(`.${this.itemClass}`).forEach((el) => {
+                el.classList.add(this.itemEnd)
+            })
+        } else {
+            el.querySelectorAll(`.${this.itemClass}`).forEach((el) => {
+                if (contentY.top <= 0) {
+                    el.classList.add(this.itemActive);
+                } else if (contentY.bottom <= this.winH) {
+                    el.classList.add(this.itemEnd)
+                }
+            })
+
         }
 
+    }
 
+    setScroll(el){
+
+        const sectionClass = el.classList[0]
+        const contentWrapper = el.querySelector(`.${sectionClass}-item`);
+        const contentWrapperScrollW = contentWrapper.scrollWidth;
+
+        el.contentWrapper = contentWrapper;
+        el.contentWrapperScrollW = contentWrapperScrollW
+
+        el.rightMax = -(el.contentWrapperScrollW - this.winW)
+
+        el.style.height = `${el.contentWrapperScrollW}px`
+        el.innerHeight = el.offsetHeight;
+        
+        el.init = true;
+        el.transformX = '0';
+        el.classList.add(`${sectionClass}-init`)
 
     }
+
+
+
+    activeScroll(el) {
+        const scrollP = this.currentScrollH - el.offsetTop;
+        const scrollCenter = scrollP / (el.innerHeight - (this.winH - this.winW))
+        const transformP = scrollCenter * el.contentWrapperScrollW;
+        let toTransform = -(transformP)
+        toTransform = Math.min(0, toTransform);
+        toTransform = Math.max(toTransform, el.rightMax)
+        el.transformX = toTransform;
+        el.contentWrapper.style.transform = `translateX(${el.transformX}px)`
+
+    }
+
 
 }
