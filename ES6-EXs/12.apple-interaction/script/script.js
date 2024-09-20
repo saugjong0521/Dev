41c7fa8ec0690944d7ba56d664ae1b32a548e19c
saugjong0521 <saugjong0521@gmail.com>
2024-09-20 20:29:56 +0900

    vault backup: 2024-09-20 20:29:56

--- ES6-EXs/12.apple-interaction/script/script.js
+++ ES6-EXs/12.apple-interaction/script/script.js
@@ -99,7 +99,7 @@
 
         for (let i = 0; i < sectionInfo[2].values.videoImageCount; i++) {
             imgItem = new Image();
-            imgItem.src = `./images/section02/00${i}.jpg`;
+            imgItem.src = `./images/section02/00${i + 1}.jpg`;
             sectionInfo[2].objs.videoImages.push(imgItem)
 
             // console.log(imgItem.src)
