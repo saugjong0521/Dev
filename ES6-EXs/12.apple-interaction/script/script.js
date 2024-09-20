01031df13398a37f89793b52350bdaf9493e2c05
saugjong0521 <saugjong0521@gmail.com>
2024-09-20 20:07:56 +0900

    vault backup: 2024-09-20 20:07:56

--- ES6-EXs/12.apple-interaction/script/script.js
+++ ES6-EXs/12.apple-interaction/script/script.js
@@ -69,7 +69,7 @@
                 imgSequence: [0, 3],    // 이미지의 프레임이기에 0~3
 
                 //첫번째 이미지
-                canvas_opacity_in_1: [0, 1, { start: 0, end: 0 }],
+                canvas_opacity_in_1: [1, 0, { start: 0.1, end: 0.2 }],
                 canvas_scale_in_1: [1, 0.9, { start: 0.1, end: 0.2 }]
 
 
