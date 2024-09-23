43e28f315c9b06cd0720f12690cc5ce7f008b0b9
saugjong0521 <149376211+saugjong0521@users.noreply.github.com>
2024-09-23 15:40:16 +0900

    Update r2e.js

--- Output/r2e_beta/r2e.js
+++ Output/r2e_beta/r2e.js
@@ -1,6 +1,7 @@
 async function displayValue() {
-    const key = document.getElementById('keyInput').value.trim().toLowerCase(); 
-    
+    const key = document.getElementById('keyInput').value.trim().toLowerCase();
+    const rewardDate = document.querySelector('.reward-date')
+
     try {
         // CSV
         const response = await fetch('r2e_data.csv?v=' + new Date().getTime(), {
@@ -9,25 +10,34 @@ async function displayValue() {
                 'Pragma': 'no-cache',
             },
         });
-        const csvData = await response.text();    
-    
+        const csvData = await response.text();
+
         const results = Papa.parse(csvData, {
             header: true,
-            skipEmptyLines: true,  
+            skipEmptyLines: true,
         });
-        
+
         const keyValuePairs = {};
         results.data.forEach(row => {
             const value1 = parseFloat(row.value1);
             const value2 = parseFloat(row.value2);
             const value3 = parseFloat(row.value3);
+            const value4 = parseFloat(row.value4);
             const lowerCaseKey = row.key.trim().toLowerCase();
-            keyValuePairs[lowerCaseKey] = `Total Reward: ${value1.toFixed(4)}\n\nEligible (Monthly) Reward: ${value2.toFixed(4)}\n\nAmount of inKSTA to send: ${value3.toFixed(4)}`;
+            keyValuePairs[lowerCaseKey] =
+`Total Rewards Left: ${value1.toFixed(4)}
+
+Claimable Monthly Rewards: ${value2.toFixed(4)}
+
+inKSTA Required: ${value3.toFixed(4)}
+
+Current inKSTA sent: ${value4.toFixed(4)}`;
         });
-    
+
         const value = keyValuePairs[key];
         if (value) {
             document.getElementById('output').textContent = value;
+            rewardDate.classList.add('display-reward');
         } else {
             document.getElementById('output').textContent = 'No Rewards Available';
         }
