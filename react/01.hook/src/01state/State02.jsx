424ae78906cbc37849f2227a8de684f8e42731d9
saugjong0521 <saugjong0521@gmail.com>
2024-09-23 21:31:01 +0900

    vault backup: 2024-09-23 21:31:01

---
+++ react/01.hook/src/01state/State02.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ChildState from "./components/ChildState";
+
+export default function State02(){
+    const [name,setName]= useState('park')
+    
+    return(
+        <>
+            <ChildState nameToss={name}/>
+            <button onClick={()=>setName('kim')}>클릭!</button>
+        </>
+    )
+}
---
+++ react/01.hook/src/01state/components/ChildState.jsx
@@ -0,0 +1,8 @@
+
+export default function ChildState(){
+    return(
+        <>
+        <p>내 이름은 {name} 입니다.</p>
+        </>
+    )
+}
