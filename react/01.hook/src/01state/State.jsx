e24918aca810eeb025475287e807d33d6851ad01
saugjong0521 <saugjong0521@gmail.com>
2024-09-24 19:16:56 +0900

    vault backup: 2024-09-24 19:16:56

--- react/01.hook/src/01state/State.jsx
+++ react/01.hook/src/01state/State.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-export default function State() {
+export default function State(){
     const [name, setName] = useState('홍길동');
     console.log(name)
     console.log(useState)
 
-    return (
+    return(
         <>
-            <p>{name}</p>
-            <button onClick={() => setName('lee')}>이름변경</button>
+        <p>{name}</p>
+        <button onClick={()=>setName('lee')}>이름변경</button>
         </>
     )
 }
--- react/01.hook/src/index.js
+++ react/01.hook/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import State from './01state/State';
+import State02 from './01state/State02';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <State/>
+    <State02/>
   </React.StrictMode>
 );
 
