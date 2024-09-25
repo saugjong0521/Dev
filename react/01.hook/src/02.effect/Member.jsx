e0dea3425c051b17033f8484673d1d0d07eaae94
saugjong0521 <saugjong0521@gmail.com>
2024-09-25 20:39:36 +0900

    vault backup: 2024-09-25 20:39:36

--- react/01.hook/src/02.effect/Member.jsx
+++ react/01.hook/src/02.effect/Member.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 
@@ -6,7 +7,7 @@ export default function Member () {
     //2
     const [item, setItem] = useState([]);
 
-    const [check, setCheck] = useState(check);
+    const [check, setCheck] = useState(false);
 
 
     //1
