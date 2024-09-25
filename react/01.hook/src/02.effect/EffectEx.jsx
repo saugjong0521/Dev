8437afa64ba3bac4e79ce29147b9c38a0bb1993d
saugjong0521 <saugjong0521@gmail.com>
2024-09-25 20:11:35 +0900

    vault backup: 2024-09-25 20:11:35

--- react/01.hook/src/02.effect/EffectEx.jsx
+++ react/01.hook/src/02.effect/EffectEx.jsx
@@ -3,8 +3,19 @@
 
 useState와 useEffect를 이용해서 카운트다운을 만들기
 
+1. useState의 초기값을 설정하고 useEffect로 값을 차감
+2. useState의 값이 0이 되면 텍스트를 화면에 출력하고, 카운트다운이 멈추도록 설정
+
 */
 
-export default function EffectEx(){
+export default function EffectEx() {
+
+    const [cdinit, setCountdown] = useState(5);
+
+    const cdCalc = ((){
+        setCountdown((prev) => prev - 1);
+    }, 1000)
+
+
 
 }
