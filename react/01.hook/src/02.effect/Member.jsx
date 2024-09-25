8ef94e0b2ca6740d1692b0fc9d8779cb53a3bd7a
saugjong0521 <saugjong0521@gmail.com>
2024-09-25 20:43:33 +0900

    vault backup: 2024-09-25 20:43:33

--- react/01.hook/src/02.effect/Member.jsx
+++ react/01.hook/src/02.effect/Member.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
@@ -6,9 +6,24 @@ import { useState } from "react";
 export default function Member () {
     //2
     const [item, setItem] = useState([]);
-
     const [check, setCheck] = useState(false);
 
+    useEffect (()=>{
+        //비동기 방식으로 데이터를 처리하는 메소드
+        /*
+        비동기 방식
+        - 작업을 즉시 완료하지 않고, 작업이 끝나는 것을 기다리지 않고 다른 작업을 처리하는 방식
+        네트워크 요청같이 시간이 오래 걸리는 작업에서 비동기 방식을 많이 채택
+
+        동기 방식
+        - 각 작업을 순서대로 처리하며, 하나의 작업이 완료될때까지 다음 작업으로 넘어가지 않음
+        - 하나의 작업이 오래 걸린다면 대기가 길어지는 단점
+        - 코드가 흐름대로 작성되기 때문에 직관적
+        */
+
+        fetch(``)
+
+    })
 
     //1
     return(
