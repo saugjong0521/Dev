faa05823de3081c05fe315b3634c1f0ebf982794
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 20:32:12 +0900

    vault backup: 2024-09-27 20:32:12

--- react/01.hook/src/04reducer/ShoppingCartReducer.jsx
+++ react/01.hook/src/04reducer/ShoppingCartReducer.jsx
@@ -1,8 +1,14 @@
 
 
+function findItemIndex(el, idx){
+    return el.findIndex((item)=>item.id === id)
+}
+
 
 export function ShoppingCartReducer(state, action) {
     switch (action.type) {
+        case 'add-item':
+            const currentIndex = 
 
     }
 }
