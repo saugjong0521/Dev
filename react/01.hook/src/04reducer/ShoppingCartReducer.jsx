59f69b3c8fe0cffce973ecf9d81dc978af6a5c6d
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 20:37:09 +0900

    vault backup: 2024-09-27 20:37:09

--- react/01.hook/src/04reducer/ShoppingCartReducer.jsx
+++ react/01.hook/src/04reducer/ShoppingCartReducer.jsx
@@ -1,14 +1,24 @@
 
 
+export const init={
+    items: [], //카트에 담긴 상품 목록들
+    totalItems: 0, //카트에 답긴 총 상품의 갯수
+    totalPrice: 0, //카트에 담긴 상품들의 총 금액
+}
+
 function findItemIndex(el, idx){
-    return el.findIndex((item)=>item.id === id)
+    return el.findIndex((item)=>item.idx === idx)
 }
 
 
 export function ShoppingCartReducer(state, action) {
     switch (action.type) {
         case 'add-item':
-            const currentIndex = findItemIndex(state.el, action.id)
+            const currentIndex = findItemIndex(state.items, action.pay.id)
+            if(currentIndex >= 0){
+                //새 상품이 아닐 경우 금액만 증가
+                const updateItems
+            }
 
     }
 }
