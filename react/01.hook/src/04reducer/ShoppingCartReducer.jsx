fd4af43391797874368c857e6459549e505be664
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 21:18:11 +0900

    vault backup: 2024-09-27 21:18:11

--- react/01.hook/src/04reducer/ShoppingCartReducer.jsx
+++ react/01.hook/src/04reducer/ShoppingCartReducer.jsx
@@ -14,39 +14,46 @@ function findItemIndex(el, idx) {
 export function ShoppingCartReducer(state, action) {
     switch (action.type) {
         case 'add-item':
-            {const currentIndex = findItemIndex(state.items, action.pay.id) //{}로 묶어서 currentIndex를 지역변수로 만듬
-            if (currentIndex >= 0) {
-                //새 상품이 아닐 경우 금액만 증가
-                const updateItems = state.items.map((item, idx) =>
-                    idx === currentIndex ? { ...item, quantity: item.quantity + 1 } : item)
+            {
+                const currentIndex = findItemIndex(state.items, action.pay.id) //{}로 묶어서 currentIndex를 지역변수로 만듬
+                if (currentIndex >= 0) {
+                    //새 상품이 아닐 경우 금액만 증가
+                    const updateItems = state.items.map((item, idx) =>
+                        idx === currentIndex ? { ...item, quantity: item.quantity + 1 } : item)
 
-                return {
-                    ...state,
-                    items: updateItems,
-                    totalPrice: state.totalPrice + action.pay.price
-                }
-            } else {
-                //새 상품일 경우 카트에 추가
-                return {
-                    ...state,
-                    items: [...state.items, { ...action.pay, quantity: 1 }],
-                    totalItems: state.totalItems + 1,
-                    totalPrice: state.totalPrice + action.pay.price
+                    return {
+                        ...state,
+                        items: updateItems,
+                        totalPrice: state.totalPrice + action.pay.price
+                    }
+                } else {
+                    //새 상품일 경우 카트에 추가
+                    return {
+                        ...state,
+                        items: [...state.items, { ...action.pay, quantity: 1 }],
+                        totalItems: state.totalItems + 1,
+                        totalPrice: state.totalPrice + action.pay.price
+                    }
                 }
             }
-        }
 
-            case 'remove-item':
-                {const currentIndex = findItemIndex(state.items, action.pay.id)
+        case 'remove-item':
+            {
+                const currentIndex = findItemIndex(state.items, action.pay.id)
+                if (currentIndex === -1) return state;
+                const removeIndex = state.items[currentIndex]
 
-
-
-                    
+                if (removeIndex.quantity > 1) {
+                    const updateItems = state.items.map((el, idx) =>
+                        idx === currentIndex ? { ...el, quantity: el.quantity - 1 } : item
+                    )
                 }
 
+            }
 
 
-            default:
-                return state
+
+        default:
+            return state
     }
 }
