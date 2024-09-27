0a0febcc55d40cfd570dc1253d1ce95d5060fcd0
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 19:39:13 +0900

    vault backup: 2024-09-27 19:39:13

--- react/01.hook/src/04reducer/Reducer.jsx
+++ react/01.hook/src/04reducer/Reducer.jsx
@@ -6,6 +6,10 @@ useReducer
 상태 관리 hook
 useState의 대체 hook
 
+useState의 경우 간단한 상태 로직에서는 유용하지만,
+복잡한 로직에서 상태관리를 할 경우 현재 상태가 이전 상태에 대한 의존도가 더 커지기 때문에
+구조가 복잡해질수록 useReducer가 더 좋다.
+
 */
 
 export default function Reducer () {
