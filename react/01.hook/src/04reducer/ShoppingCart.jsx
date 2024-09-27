387ec861f17407bb9b6701f71b869ea4360094b4
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 20:28:10 +0900

    vault backup: 2024-09-27 20:28:10

--- react/01.hook/src/04reducer/ShoppingCart.jsx
+++ react/01.hook/src/04reducer/ShoppingCart.jsx
@@ -2,32 +2,32 @@
 
 
 
-export default function ShoppingCart(){
+export default function ShoppingCart() {
     const item = [
-        {id: 1,name: 'item01', price: 5000},
-        {id: 2,name: 'item02', price: 5000},
-        {id: 3,name: 'item03', price: 5000},
-        {id: 4,name: 'item04', price: 5000},
-        {id: 5,name: 'item05', price: 5000},
-        {id: 6,name: 'item06', price: 5000},
-        {id: 7,name: 'item07', price: 5000},
-        {id: 8,name: 'item08', price: 5000},
-        {id: 9,name: 'item09', price: 5000},
-        {id: 10,name: 'item10', price: 5000},
+        { id: 1, name: 'item01', price: 5000 },
+        { id: 2, name: 'item02', price: 5000 },
+        { id: 3, name: 'item03', price: 5000 },
+        { id: 4, name: 'item04', price: 5000 },
+        { id: 5, name: 'item05', price: 5000 },
+        { id: 6, name: 'item06', price: 5000 },
+        { id: 7, name: 'item07', price: 5000 },
+        { id: 8, name: 'item08', price: 5000 },
+        { id: 9, name: 'item09', price: 5000 },
+        { id: 10, name: 'item10', price: 5000 },
     ]
 
 
     return (
         <>
-        <h1>아이템 리스트</h1>
-        {item.map((el)=>(
-            <li key={el.id}>
-                <p>{el.name}</p>
-                <p>{el.price}</p>
-                <button>장바구니 추가</button>
-                <button>장바구니 삭제</button>
-            </li>
-        ))}
+            <h1>아이템 리스트</h1>
+            {item.map((el) => (
+                <li key={el.id}>
+                    <p>{el.name}</p>
+                    <p>{el.price}</p>
+                    <button>장바구니 추가</button>
+                    <button>장바구니 삭제</button>
+                </li>
+            ))}
         </>
     )
 
