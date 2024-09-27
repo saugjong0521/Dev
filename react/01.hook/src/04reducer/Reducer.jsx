fa00b48528d0b51ff4549bd3bb572c1a11b534ac
saugjong0521 <saugjong0521@gmail.com>
2024-09-27 19:40:14 +0900

    vault backup: 2024-09-27 19:40:14

--- react/01.hook/src/04reducer/Reducer.jsx
+++ react/01.hook/src/04reducer/Reducer.jsx
@@ -10,6 +10,10 @@ useState의 경우 간단한 상태 로직에서는 유용하지만,
 복잡한 로직에서 상태관리를 할 경우 현재 상태가 이전 상태에 대한 의존도가 더 커지기 때문에
 구조가 복잡해질수록 useReducer가 더 좋다.
 
+주로 reducer에서는 switch문을 자주 사용한다.
+여러가지 액션의 유형을 간단하게 처리할 수 있기 때문에 가독성 면에서
+if문보다 switch의 사용도가 높은 편
+
 */
 
 export default function Reducer () {
