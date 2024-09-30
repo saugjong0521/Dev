e759148d43ad1322e00252c01510c5734956ee3a
saugjong0521 <saugjong0521@gmail.com>
2024-09-30 21:20:59 +0900

    vault backup: 2024-09-30 21:20:59

--- react/01.hook/src/06context/Darkmode.jsx
+++ react/01.hook/src/06context/Darkmode.jsx
@@ -11,8 +11,14 @@ export default function Darkmode(){
 
     const [theme, setTheme] = useState('dark'); // 기본 세팅 값 설정
 
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light' )
+    }
+
+
     return (
-        <ThemeContext.Provider value = {theme}>
+        <ThemeContext.Provider value = {{theme, toggleTheme}}>
         {/* Provider = context로 생성된 객체 안에 포함되는 컴포넌트들을 관리할 태그 */}
         <DarkmodeBtn/>
 
