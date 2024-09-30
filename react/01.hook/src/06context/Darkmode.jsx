f8e771aa546ec1a373b78efe9fabb3ef604110ce
saugjong0521 <saugjong0521@gmail.com>
2024-09-30 21:12:00 +0900

    vault backup: 2024-09-30 21:12:00

--- react/01.hook/src/06context/Darkmode.jsx
+++ react/01.hook/src/06context/Darkmode.jsx
@@ -1,7 +1,9 @@
 //다크모드 만들기
 
-import { useState } from "react"
+import { createContext, useState } from "react"
 
+
+export const ThemeContext = createContext();    // 밖으로 값 전달
 
 
 export default function Darkmode(){
@@ -9,6 +11,6 @@ export default function Darkmode(){
     const [theme, setTheme] = useState('dark'); // 기본 세팅 값 설정
 
     return (
-
+        {/* context로 생성된 객체 안에 포함되는 컴포넌트 */}
     )
 }
