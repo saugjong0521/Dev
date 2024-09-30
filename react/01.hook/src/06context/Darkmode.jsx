7beca9888b98047357b4219ed996a7632bc76d1e
saugjong0521 <saugjong0521@gmail.com>
2024-09-30 21:09:01 +0900

    vault backup: 2024-09-30 21:09:01

--- react/01.hook/src/06context/Darkmode.jsx
+++ react/01.hook/src/06context/Darkmode.jsx
@@ -1,7 +1,14 @@
+//다크모드 만들기
 
+import { useState } from "react"
 
 
 
 export default function Darkmode(){
-    
+
+    const [theme, setTheme] = useState('dark'); // 기본 세팅 값 설정
+
+    return (
+
+    )
 }
