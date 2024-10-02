8133eb58666aebbad4c9fcba05bb121050fa1876
saugjong0521 <saugjong0521@gmail.com>
2024-10-02 19:45:22 +0900

    vault backup: 2024-10-02 19:45:22

--- react/02.css/src/02.styled-components/StyledCom.jsx
+++ react/02.css/src/02.styled-components/StyledCom.jsx
@@ -1,4 +1,12 @@
+/*
 
+styled-components
+//yarn add styled-components
+//npm install styled-components
+
+설치형 라이브러리로 스크립트 코드 안에서 css를 작성할 수 있게 해주는 라이브러리
+
+*/
 
 export default function StyledCom (){
     return (
