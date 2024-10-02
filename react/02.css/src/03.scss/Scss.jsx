903e41afe089559f400d5dacd05b7d50c0d7470c
saugjong0521 <saugjong0521@gmail.com>
2024-10-02 21:43:18 +0900

    vault backup: 2024-10-02 21:43:18

--- react/02.css/src/03.scss/Scss.jsx
+++ react/02.css/src/03.scss/Scss.jsx
@@ -4,10 +4,10 @@ export default function Scss() {
     return(
         <div className="container">
             <div className="box">box</div>
-            <div className="box">box</div>
-            <div className="box">box</div>
-            <div className="box">box</div>
-            <div className="box">box</div>
+            <div className="box orange">box</div>
+            <div className="box pink">box</div>
+            <div className="box cyan">box</div>
+            <div className="box purple">box</div>
         </div>
     )
 }
