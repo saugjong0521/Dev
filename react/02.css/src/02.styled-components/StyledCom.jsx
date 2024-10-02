c38b1525920ee0cd0d91cf866a3d0c205fca4af0
saugjong0521 <saugjong0521@gmail.com>
2024-10-02 19:48:22 +0900

    vault backup: 2024-10-02 19:48:22

--- react/02.css/src/02.styled-components/StyledCom.jsx
+++ react/02.css/src/02.styled-components/StyledCom.jsx
@@ -10,13 +10,14 @@ styled-components
 
 스타일과 로직을 한 파일에서 관리하는 특징이 있다.
 
+컴포넌트명을 작성할때 소문자로 작성하면 태그로 인식하기 때문에, 대문자로 작성한다.
 */
 
 export default function StyledCom (){
     return (
         <>
         <Container>
-            
+
         </Container>
         </>
     )
