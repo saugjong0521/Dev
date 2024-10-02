893028fc94059809767388bb97244ac9c3d29f92
saugjong0521 <saugjong0521@gmail.com>
2024-10-02 20:43:20 +0900

    vault backup: 2024-10-02 20:43:20

--- react/02.css/src/02.styledComponents/StyledCom.jsx
+++ react/02.css/src/02.styledComponents/StyledCom.jsx
@@ -56,8 +56,7 @@ const ButtonItem = styled(({fontColor, ...props}) => <button{...props}/>)`
     (props) => props.fontColor는 현재 컴포넌트에 전달된 props객체에서 값을 전달받아 styles에 적용하는 방식
     */
 
-    ${(props)=>props.sub && css `background: cyan`};
-    
+    ${(props)=>props.sub && css `background: cyan;`}
     //css를 조건부로 넘겨줄때 css를 체크하는 메소드, styled-components에서 추가로 import
     &:hover{
         opacity: 1;
