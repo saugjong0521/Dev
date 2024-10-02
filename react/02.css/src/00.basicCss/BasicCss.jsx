c2d137c390aab2c5b2967eab2903b2b837a58b29
saugjong0521 <saugjong0521@gmail.com>
2024-10-02 19:15:32 +0900

    vault backup: 2024-10-02 19:15:32

---
+++ react/02.css/src/00.basicCss/BasicCss.jsx
@@ -0,0 +1,15 @@
+import './BasicCss.css'
+
+
+export default function BasicCss() {
+
+    return (
+        
+        <div className="container">
+
+            <h1 className="title">리액트 기본 css 작성 방식</h1>
+
+        </div>
+
+    )
+}
