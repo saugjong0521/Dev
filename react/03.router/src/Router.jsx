45719a333587640f3a2babe4c019edb116fce335
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 20:17:41 +0900

    vault backup: 2024-10-06 20:17:40

---
+++ react/03.router/src/Router.jsx
@@ -0,0 +1,21 @@
+
+
+
+export default function Router (){
+    /*
+    react router-dom
+    
+    react의 클라이언트 측에서 라우팅을 처리하기 위한 라이브러리
+    spa에서 url을 사용해서 여러 페이지처럼 보이게 하고, 페이지간의 탐색을 가능하게 한다.
+    각 페이지는 실제로 다른 html 파일이 아닌, 하나의 html파일 내에서 스크립트로 url 방식에 따라 랜더링이 되는 방식
+
+    라우터 안에서 사용하는 컴포넌트
+    BrowserRouter : 브라우저 주소를 변경하고 url을 관리하는 역할의 컴포넌트
+    BrowserRouter는 앱을 감싸고 그 안에서 라우팅이 이루어 진다.
+
+    Route / Routes
+    Route는 각 경로로 이동시켜주는 컴포넌트
+    Routes는 여러개의 Route를 감싸는 요소
+
+    */
+}
