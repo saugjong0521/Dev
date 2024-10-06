42bd2ab628515d1921e49c9fea4962ab824acc61
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 22:44:40 +0900

    vault backup: 2024-10-06 22:44:40

--- react/03.router/src/pages/Login.jsx
+++ react/03.router/src/pages/Login.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 
 
 export default function Login () {
+
+    const [userName, setUserName] = useState('');
+
     return (
         <>
             <h1>로그인 페이지</h1>
             <input type="text" 
                 value={userName} 
                 onChange={(e)=>setUserName(e.target.value)}/>
+            <button onClick={handleLogin}>로그인</button>
         </>
     )
 }
