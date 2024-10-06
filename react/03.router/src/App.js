9dc6d7959d795d7caca2278fc1105e27e041f4a7
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 20:41:41 +0900

    vault backup: 2024-10-06 20:41:41

--- react/03.router/src/App.js
+++ react/03.router/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import {Router} from 'react-router-dom'
+import Header from './pages/Header';
+import Footer from './pages/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Router>
-
+        <Header/>
+        <Home/>
+        <Footer/>
       </Router>
     </>
   );
