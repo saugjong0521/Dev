3bd68fa9e4b55c6a53421834d6bf2e8b30a7c0b0
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 22:51:40 +0900

    vault backup: 2024-10-06 22:51:40

--- react/03.router/src/pages/Login.jsx
+++ react/03.router/src/pages/Login.jsx
@@ -2,6 +2,11 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 
 
+//useNavigate = 강제적으로 라우팅을 하게 해주는 훅
+//이벤트 이후나 조건부에서 특정 path로 이동시킬때 사용한다.
+
+//ex) 로그인이 성공하면 메인 페이지로 이동
+//ex) 회원가입 후, 로그인 페이지로 이동
 export default function Login () {
 
     const navigate = useNavigate()
