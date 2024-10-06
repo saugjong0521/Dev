142b336614bd59f3aa04eb39d5528502faa489bf
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 21:55:14 +0900

    vault backup: 2024-10-06 21:55:14

--- react/03.router/src/pages/ItemDetail.jsx
+++ react/03.router/src/pages/ItemDetail.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom"
 
 
 export default function ItemDetail (){
+
+    const {id} = useParams();
+    console.log(id)
+    //path에서 전달된 id를 useParams로 전달받음
 
     return (
         <>
