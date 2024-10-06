eeda27f83c199327438eab68694f23dc5615f48e
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 22:41:39 +0900

    vault backup: 2024-10-06 22:41:39

--- react/03.router/src/pages/Header.jsx
+++ react/03.router/src/pages/Header.jsx
@@ -16,6 +16,8 @@ export default function Header () {
 
                 <li><a href="https://www.naver.com">네이버</a></li>
                 {/* 리액트에서도 a태그를 사용할 수 있지만, 외부링크를 참조할 때에만 사용한다. */}
+
+                <li><Link to='/login'>로그인</Link></li>
             </ul>
         </nav>
         </>
