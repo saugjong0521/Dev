a32a38129ecd13dec5ed19c4d621829a15a20fb4
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 20:53:41 +0900

    vault backup: 2024-10-06 20:53:41

--- react/03.router/src/App.js
+++ react/03.router/src/App.js
@@ -4,6 +4,9 @@ import {Route, Router, Routes} from 'react-router-dom'
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ItemList from './pages/ItemList';
 
 function App() {
   return (
@@ -14,7 +17,9 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/' element={<About/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/products' element={<Products/>}/>
+            <Route path='/itemList' element={<ItemList/>}/>
           </Routes>
         </main>
 
