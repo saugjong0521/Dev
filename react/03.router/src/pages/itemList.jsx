467cb464dc6fe7561dc693c552a9054bb1b1b84c
saugjong0521 <saugjong0521@gmail.com>
2024-10-06 20:50:41 +0900

    vault backup: 2024-10-06 20:50:41

---
+++ react/03.router/src/pages/itemList.jsx
@@ -0,0 +1,27 @@
+
+
+export default function ItemList (){
+
+    const items = [
+        {id : 1, name: item1},
+        {id : 2, name: item2},
+        {id : 3, name: item3},
+        {id : 4, name: item4},
+        {id : 5, name: item5},
+    ]
+
+
+    return (
+        
+        <>
+            <h1>Products Page</h1>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+        </>
+
+    )
+
+}
