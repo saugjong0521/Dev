491988129fe78c27a8bae0e5fd995448c9ddf4b7
saugjong0521 <saugjong0521@gmail.com>
2024-10-09 21:05:39 +0900

    vault backup: 2024-10-09 21:05:39

---
+++ Output/canvas/script.js
@@ -0,0 +1,106 @@
+window.onload = () => {
+    drawing();
+    resizeCanvas();
+}
+
+//캔버스 크기 조정
+function resizeCanvas() {
+    const canvas = document.querySelector('#drawing');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function drawing() {
+    const canvas = document.querySelector('#drawing');
+    const ctx = canvas.getContext('2d');
+    let drawing = false;
+    let color = 'black';  // 초기 색상은 검정색
+    let brush = 1;
+
+    // 시작 좌표
+    let startX = 0;
+    let startY = 0;
+
+    // 마우스를 눌렀을 때 그리기 시작
+    canvas.addEventListener('mousedown', (e) => {
+        drawing = true;
+        startX = e.offsetX;
+        startY = e.offsetY;
+    });
+
+    // 마우스를 이동할 때 그리기
+    canvas.addEventListener('mousemove', (e) => {
+        if (drawing) {
+            drawLine(ctx, startX, startY, e.offsetX, e.offsetY);
+            startX = e.offsetX;
+            startY = e.offsetY;
+        }
+    });
+
+    // 마우스를 떼면 그리기 종료
+    canvas.addEventListener('mouseup', () => {
+        drawing = false;
+    });
+
+    // 선을 그리는 함수
+    function drawLine(context, x1, y1, x2, y2) {
+        drawThickness(); //굵기 변경 함수 호출
+        changeColor();  // 색상 변경 함수 호출
+
+        context.strokeStyle = color; // 현재 선택된 색상으로 선을 그림
+        context.lineWidth = brush*10;       // 선 두께 설정
+        context.lineCap = "round"
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+        
+    }
+
+
+    
+
+    // 색상 변경 처리
+    function changeColor() {
+        const colorItems = document.querySelectorAll('.colorSelect');
+
+        // 각 색상 항목에 클릭 이벤트 추가
+        colorItems.forEach(item => {
+            item.addEventListener('click', () => {
+                const bgColor = window.getComputedStyle(item).getPropertyValue('background-color');
+                color = bgColor; // 선택된 배경색을 색상으로 설정
+            });
+        });
+    }
+
+    //굵기 변경 처리
+    function drawThickness(){
+        const brushItem = document.querySelector('.thickness')
+        brush = brushItem.value
+    }
+
+    //리셋 처리
+    function resetCanvas(){
+        const resetBtn = document.querySelector('.clearBtn')
+        
+        resetBtn.addEventListener('click', () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+        })
+    }
+    resetCanvas();
+
+    //저장 처리
+    function saveCanvas(){
+        const saveBtn = document.querySelector('.saveBtn')
+
+        saveBtn.addEventListener('click',()=>{
+            const save = canvas.toDataURL('image/png',1.0); //(저장형식,품질)
+            console.log(save)
+            alert(save)
+        })
+    }
+    saveCanvas();
+
+}
