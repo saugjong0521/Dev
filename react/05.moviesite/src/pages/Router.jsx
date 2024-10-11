affe23048720c6e7547ce5c4fe122420140269c7
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:27:01 +0900

    vault backup: 2024-10-11 19:27:01

--- react/05.moviesite/src/pages/Router.jsx
+++ react/05.moviesite/src/pages/Router.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
+import NotFound from "./NotFound";
+import Main from "./Main";
 
 
 
 export default function Router(){
     const router = createBrowserRouter({
         {
-            path: '/'
-            element : <Home/>
+            path: '/',
+            element : <Home/>,
+            errorElement : <NotFound/>,
+            children : [
+                {index: true, element: <Main/>}
+            ],
         }
 
     })
