302945890d2a5de48224f70088f9fd2e7477e871
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:41:02 +0900

    vault backup: 2024-10-11 19:41:02

---
+++ react/05.moviesite/src/components/NavigationBar.jsx
@@ -0,0 +1,20 @@
+
+
+export default function NavigationBar (){
+
+    return(
+        <Nav>
+            <ul>
+                <li><Link to='/'>홈</Link></li>
+                <li><Link to='/'>시리즈</Link></li>
+                <li><Link to='/'>영화</Link></li>
+                <li><Link to='/'>NEW!요즘 대세 콘텐츠</Link></li>
+                <li><Link to='/'>내가 찜한 리스트</Link></li>
+                <li><Link to='/'>언어별로 찾아보기</Link></li>
+            </ul>
+
+        </Nav>
+
+    )
+
+}
