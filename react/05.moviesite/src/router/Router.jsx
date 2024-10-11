2cae1205285aa51bc74bf129c31fe305db4c3624
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:32:01 +0900

    vault backup: 2024-10-11 19:32:01

--- react/05.moviesite/src/router/Router.jsx
+++ react/05.moviesite/src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Home";
-import NotFound from "./NotFound";
-import Main from "./Main";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import Main from "../pages/Main";
 
 
 
@@ -10,14 +10,14 @@ export default function Router(){
         {
             path: '/',
             element : <Home/>,
-            errorElement : <NotFound/>,
-            children : [
-                {index: true, element: <Main/>}
+            errorElement : <NotFound/>, //일치하는 경로가 없을때 출력할 컴포넌트(에러페이지)
+            children : [    //자식 경로 설정
+                {index: true, element: <Main/>} //main컴퍼넌트를 기본경로로 설정
             ],
         }
 
     ])
 
-    return <RouterProvider router = {router} s/>
+    return <RouterProvider router = {router}/>
 
 }
