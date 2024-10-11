0ceb245fe2cf82997a90e660473173a708abd838
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 20:12:59 +0900

    vault backup: 2024-10-11 20:12:59

--- react/05.moviesite/src/components/Search.jsx
+++ react/05.moviesite/src/components/Search.jsx
@@ -1,18 +1,30 @@
 import { BsSearch } from "react-icons/bs";
 
+/*
+framer-motion
+=> yarn add framer-motion
 
+react에서 사용하는 애니메이션 생성 라이브러리
+리액트의 특성상 생명주기를 기반으로 하는 컴포넌트를 불러오는 방식이기 때문에, 애니메이션이 연결될때
+일반적으로 사용하는 css속성을 기반으로 컨트롤
+
+*/
 
 
 export default function Search (){
 
     return(
 
-        <>
-            <button type="button" className="search-btns">
-                <BsSearch />
-            </button>
-            
-        </>
+            <SearchForm>
+                
+                
+                <button type="button" className="search-btns">
+                    <BsSearch />
+                </button>
+
+
+
+            </SearchForm>
 
     )
 
