62e626f246b4273271c61b116cbe1a574b62b111
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:23:04 +0900

    vault backup: 2024-10-11 19:23:04

--- react/05.moviesite/src/pages/Home.jsx
+++ react/05.moviesite/src/pages/Home.jsx
@@ -4,7 +4,6 @@ export default function Home (){
 
     return(
         <>
-            <h1>메인 페이지</h1>
         </>
 
     )
---
+++ react/05.moviesite/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Main (){
+ 
+    return(
+        <>
+        
+        <h1>메인 페이지</h1>
+        </>
+
+    )
+
+}
