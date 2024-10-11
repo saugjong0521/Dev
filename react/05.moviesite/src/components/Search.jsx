9b80faabf9aece4bd28e7b3786e256f73fe51047
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 20:16:04 +0900

    vault backup: 2024-10-11 20:16:04

--- react/05.moviesite/src/components/Search.jsx
+++ react/05.moviesite/src/components/Search.jsx
@@ -10,6 +10,10 @@ react에서 사용하는 애니메이션 생성 라이브러리
 리액트의 특성상 생명주기를 기반으로 하는 컴포넌트를 불러오는 방식이기 때문에, 애니메이션이 연결될때
 일반적으로 사용하는 css속성을 기반으로 컨트롤
 
+사용하는 속성
+initial = 초기값
+animate = 컴포넌트가 동적인 상태를 통해서 최종적으로 변경될 속성 값
+
 */
 
 
@@ -18,7 +22,10 @@ export default function Search (){
     return(
 
             <SearchForm>
-                <motion.div>
+                <motion.div
+                    initial = {{width : 50}}
+                    animate = {{width : 250}}
+                >
                     
                     <button type="button" className="search-btns">
                         <BsSearch />
