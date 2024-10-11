ec2a53a54b995f47985ba0c78985ca0c6588067a
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:31:00 +0900

    vault backup: 2024-10-11 19:31:00

--- react/05.moviesite/src/index.js
+++ react/05.moviesite/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
 import GlobalStyle from './style/GlobalStyle';
-import Router from './pages/Router';
+import Router from './router/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
