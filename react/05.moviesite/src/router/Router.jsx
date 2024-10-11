aecf71b3147e2308f8e2ebeb85564cc0bc896f47
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:33:03 +0900

    vault backup: 2024-10-11 19:33:03

--- react/05.moviesite/src/router/Router.jsx
+++ react/05.moviesite/src/router/Router.jsx
@@ -12,12 +12,12 @@ export default function Router(){
             element : <Home/>,
             errorElement : <NotFound/>, //일치하는 경로가 없을때 출력할 컴포넌트(에러페이지)
             children : [    //자식 경로 설정
-                {index: true, element: <Main/>} //main컴퍼넌트를 기본경로로 설정
+                {index: true, element: <Main/>} //main 컴포넌트를 기본경로로 설정
             ],
         }
 
     ])
 
-    return <RouterProvider router = {router}/>
+    return <RouterProvider router = {router}/>  //RouterProvider로 생성된 router를 전체 페이지에 뿌림
 
 }
