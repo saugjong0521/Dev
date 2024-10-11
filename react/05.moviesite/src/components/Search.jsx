5301005082b8fb713a8e6c9c08a876acd8f47288
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 21:26:00 +0900

    vault backup: 2024-10-11 21:25:59

--- react/05.moviesite/src/components/Search.jsx
+++ react/05.moviesite/src/components/Search.jsx
@@ -59,9 +59,9 @@ export default function Search (){
 
                 //위 조건이 모두 만족이 되면 setVisible(false) 실행
             }
-            document.addEventListener('click', handleClickOutside)
+            document.addEventListener('mousedown', handleClickOutside)
             return () => {
-                document.addEventListener('click', handleClickOutside)
+                document.addEventListener('mousedown', handleClickOutside)
             }
         }
     },[keyword])
