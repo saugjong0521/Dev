b4f9b9f4dbbec64539d9f5077bdc31304c85bdba
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:11:57 +0900

    vault backup: 2024-10-11 19:11:57

--- react/05.moviesite/src/components/Header.jsx
+++ react/05.moviesite/src/components/Header.jsx
@@ -4,7 +4,11 @@ export default function Header () {
     return(
 
         <HeaderContainer>
-            
+            <Logo>
+                <Link to = '/'>
+                    넷플릭스
+                </Link>
+            </Logo>
         </HeaderContainer>
 
     )
