8b9b47866a64e19c3e6a5230708dc1203bc981c4
saugjong0521 <saugjong0521@gmail.com>
2024-10-11 19:13:57 +0900

    vault backup: 2024-10-11 19:13:57

--- react/05.moviesite/src/components/Header.jsx
+++ react/05.moviesite/src/components/Header.jsx
@@ -18,4 +18,12 @@ const HeaderContainer = styled.header`
     position: fixed;
     top: 0;
     left: 0;
+    display: flex;
+    padding: 12px 36px;
+    box-sizing: border-box;
+    background-color: black;
+    width: 100%;
+    z-index: 99;    //fixed
 `
+
+const logo
