d0046f21892bce6466b9e2636bd1ac3545fd844a
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 20:16:50 +0900

    vault backup: 2024-10-14 20:16:50

--- react/05.moviesite/src/axios/Axios.jsx
+++ react/05.moviesite/src/axios/Axios.jsx
@@ -18,8 +18,15 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 보통 API KEY같은 경우 개인 고유번호이기 때문에 노출되지 않아야 하지만, 현재 상태로는
 서버에 올릴경우 API KEY가 그대로 노출되기 때문에 보통은 환경변수로 분리해서 작업하는 방향을 선택.
 
-
-
 */
 
-const instance = axios.
+const instance = axios.create({
+    //axios.create() = 새로운 axios 인스턴스를 생성해서 외부 api와 연결
+    //변수를 해당 컴포넌트 내부에서 호출하게 되면, 연결한 api와 통신이 시작
+
+    baseURL : BASE_URL,
+    params : {
+        api_key : API_KEY,
+        language : 'ko-KR'  //기본 통신 언어 설정
+    }
+})
--- react/05.moviesite/src/pages/Main.jsx
+++ react/05.moviesite/src/pages/Main.jsx
@@ -4,9 +4,17 @@ export default function Main (){
  
     return(
         <>
-            <h1>메인 페이지</h1>
+            <MovieList>
+
+
+
+            </MovieList>
         </>
 
     )
 
 }
+
+const MovieList = style.div`
+    
+`
