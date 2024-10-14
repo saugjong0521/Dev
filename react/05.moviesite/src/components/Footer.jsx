76c66b86dea6d48dab23f46766ba917f058f7800
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 19:05:53 +0900

    vault backup: 2024-10-14 19:05:52

--- react/05.moviesite/src/components/Footer.jsx
+++ react/05.moviesite/src/components/Footer.jsx
@@ -1,11 +1,18 @@
+import { FaFacebook, FaInstagram  } from "react-icons/fa";
 
 
 export default function Footer (){
 
     return (
         <FooterContainer>
+            <SocialList>
+                <li><a href="#" target="_blank"><FaFacebook /></a></li>
+                <li><a href="#" target="_blank"><FaFacebook /></a></li>
+                <li><a href="#" target="_blank"><FaFacebook /></a></li>
+                <li><a href="#" target="_blank"><FaFacebook /></a></li>
+            </SocialList>
 
-            
+
         </FooterContainer>
     )
 }
