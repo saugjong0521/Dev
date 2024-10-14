e1a10842df05ab65d9db9515ff24e274ed1a55bb
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 21:26:51 +0900

    vault backup: 2024-10-14 21:26:51

--- react/05.moviesite/src/components/MainVideo.jsx
+++ react/05.moviesite/src/components/MainVideo.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
+import { getMovies } from "../axios/Axios";
 
 
 export default function MainVideo () {
+
+    const [videoKey, setVideoKey] = useState(null);
+    const [randomMovie, setRandomMovie] = useState('');
+
+    useEffect(() => {
+        async function MovieData(){
+            try {
+                const movies = await getMovies('now_playing')
+            }
+        }
+
+    }, [])
 
     return (
         <>
