cb0fbf0889d4a1c3e4bbaef513f816ad620ba4ad
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 21:43:49 +0900

    vault backup: 2024-10-14 21:43:49

--- react/05.moviesite/src/components/MainVideo.jsx
+++ react/05.moviesite/src/components/MainVideo.jsx
@@ -20,6 +20,7 @@ export default function MainVideo () {
                 setRandomMovie(randomVideo);
                 const videos = await getMainVideos(randomVideo.id);
                 
+                console.log(videos[0].videoKey)
             } catch (error){
                 console.log(error)
             }
