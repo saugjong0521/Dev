31218885bdc4102a538c9518d520d8e8a22e0710
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 20:29:14 +0900

    vault backup: 2024-10-14 20:29:13

--- react/05.moviesite/src/pages/Main.jsx
+++ react/05.moviesite/src/pages/Main.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
 import styled from "styled-components"
+import { getMovies } from "../axios/Axios";
 
 
 export default function Main (){
- 
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        const fetchMovies = async () => {
+            const res = await getMovies('action')
+            if( res && res.data ){
+                console.log(res.data)
+            }
+        }
+    })
+    
+
     return(
         <>
             <MovieList>
