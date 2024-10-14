413a7e6e38689792e966c403a93c8f3038af2a20
saugjong0521 <saugjong0521@gmail.com>
2024-10-14 19:07:49 +0900

    vault backup: 2024-10-14 19:07:49

--- react/05.moviesite/src/pages/Home.jsx
+++ react/05.moviesite/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "./Main";
 
@@ -8,6 +9,7 @@ export default function Home (){
         <>
             <Header/>
             <Main/>
+            <Footer/>
         </>
 
     )
