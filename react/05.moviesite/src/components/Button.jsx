ef27915ccc3d0b0918bf4d3a742e47e56eeabaa7
saugjong0521 <saugjong0521@gmail.com>
2024-10-16 19:29:28 +0900

    vault backup: 2024-10-16 19:29:28

--- react/05.moviesite/src/components/Button.jsx
+++ react/05.moviesite/src/components/Button.jsx
@@ -16,5 +16,14 @@ export default function Button (accent, children, onclick){
 
 const Btn = styled.div`
     background-color: ${(props) => props.accent ? `rgba(255,255,255,0.7)` : `rgba(100,100,100,0.8)`};
-    color: ${(props) => props.account ? `#000`, '#fff'}
+    color: ${(props) => props.account ? `#000` : `#fff`}   ;
+    padding: 12px 24px;
+    display: flex;
+    align-items: center;
+    border-radius: 40px;
+    font-size: 24px;
+    font-weight: 500;
+    &:hover {
+        background-color : ${(props)=>props.accent ? `rgba(255,255,255,0.7)` : `rgba(100,100,100,0.8)`}
+    }
 `
