8f22c1fa9b6e54a53f0c02dd718ecf081e13a903
saugjong0521 <saugjong0521@gmail.com>
2024-10-16 19:24:41 +0900

    vault backup: 2024-10-16 19:24:41

---
+++ react/05.moviesite/src/components/Button.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Button (accent, children, onclick){
+
+    return(
+
+        <Btn accent={accent} onclick = {onlick}>
+            {children}
+        </Btn>
+
+
+    )
+
+}fqq
