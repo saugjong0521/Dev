0e62b982fdd84fa0f9c47b7111221b66785f97df
saugjong0521 <saugjong0521@gmail.com>
2024-10-16 20:18:08 +0900

    vault backup: 2024-10-16 20:18:07

--- react/05.moviesite/src/components/MovieSlider.jsx
+++ react/05.moviesite/src/components/MovieSlider.jsx
@@ -1,14 +1,19 @@
 import { useEffect } from "react"
+import { getGenre } from "../axios/Axios";
 
 
 export default function MovieSlider(){
     
+    const [genres,setGenres = useState({})]
+
     useEffect(()=> {
         const fetchGenres =async () =>{
             const genreData =awsit getGenre();
             const genreMap =genreData.reduce((acc,genre)=>{
                 acc[genre.id] =genre.name
-            })
+                return acc;
+            },[])
+
         }
     })
     
