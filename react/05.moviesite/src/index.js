d6f8d65ff97defded2e966d20699cdefc05d288d
saugjong0521 <saugjong0521@gmail.com>
2024-10-16 21:08:01 +0900

    vault backup: 2024-10-16 21:08:01

--- react/05.moviesite/src/index.js
+++ react/05.moviesite/src/index.js
@@ -6,12 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
 import GlobalStyle from './style/GlobalStyle';
 import Router from './router/Router';
+import { QueryClient, QueryClientProvider} from 'react-query'
 
+const queryClient = new QueryClient ()//새로운 쿼리 클라이언트를 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <GlobalStyle/>
     <Router/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
