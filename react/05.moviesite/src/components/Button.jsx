d9888b1b9b539b839ff799c5358710697a0db0c0
saugjong0521 <saugjong0521@gmail.com>
2024-10-16 19:27:29 +0900

    vault backup: 2024-10-16 19:27:29

--- react/05.moviesite/src/components/Button.jsx
+++ react/05.moviesite/src/components/Button.jsx
@@ -1,14 +1,20 @@
+import styled from "styled-components"
 
 
 export default function Button (accent, children, onclick){
 
     return(
 
-        <Btn accent={accent} onclick = {onlick}>
+        <Btn accent={accent} onclick = {onclick}>
             {children}
         </Btn>
 
 
     )
 
-}fqq
+}
+
+const Btn = styled.div`
+    background-color: ${(props) => props.accent ? `rgba(255,255,255,0.7)` : `rgba(100,100,100,0.8)`};
+    color: ${(props) => props.account ? `#000`, '#fff'}
+`
