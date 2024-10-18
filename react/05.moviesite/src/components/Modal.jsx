3380f116eb9e0287b33ead58cd1f01bc2281b547
saugjong0521 <saugjong0521@gmail.com>
2024-10-18 21:44:20 +0900

    vault backup: 2024-10-18 21:44:20

--- react/05.moviesite/src/components/Modal.jsx
+++ react/05.moviesite/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { getModalDetail } from "../axios/Axios";
+import { useQuery } from "react-query";
 
 
 export default function Modal(movie, type){
@@ -9,17 +10,59 @@ export default function Modal(movie, type){
 
     const mediaType = movie.media_type === 'tv';
 
-    const {data, isLoading, error} = 
+    const {data, isLoading, error} = useQuery()
         ['detail', mediaType],
-        () => getModalDetail
+        () => getModalDetail(movie.id, mediaType ? 'tv' : 'movie')
         stateModalDetail ()
 
     return (
-        
-        <>
-        
-        </>
+        /*
+        AnimatePresence
+        framer-motion에서 제공하는 애니메이션 컴포넌트로 마운트와 언마운트시 애니메이션을 정용할 수 있게 해주는 컴포넌트(기본적으로 리액트에서는 페이지가 변경될때 애니메이션을 적용할 수 없다.)
+        */
+        <AnimatiePresence>
+            {movieId > (
+                <Modal animate={{opacity : 1}} exit = {{opacity: 0}}
+                <motion.div className = 'modalContent'
+                transition = {{durnation : 0.5}}>
+                    <div className="modalBg">
+                        <img src = `https://image.org/t/p/w500${movie.backdrop_path}`/>
+                    </div>
+                </motion.div>
+            )}
+        </AnimationPresence>
 
-    )
+    ):null)
+    
 
 }
+
+const ModalContainer = styled(motion.div)`
+    position: flxed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    max-height: 100vh;
+    max-height: 100vh;
+    background-color: rgba(0,0,0,0.6);
+    z-index: 99;
+    opacity: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 0;
+    box-sizing: border-box;
+    
+    
+.modatContent{
+    position: relative;
+    width: 50%;
+    max-height: 100vh;
+    background: gray;
+    z-index: 999;
+    overflow: hidden;
+}
+
+    `
+    
