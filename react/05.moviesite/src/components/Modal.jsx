00b3271794e42a28dcbb50408ff1989e0d8332a2
saugjong0521 <saugjong0521@gmail.com>
2024-10-18 21:29:21 +0900

    vault backup: 2024-10-18 21:29:21

--- react/05.moviesite/src/components/Modal.jsx
+++ react/05.moviesite/src/components/Modal.jsx
@@ -1,7 +1,12 @@
+import { useNavigate, useParams } from "react-router-dom"
 
 
 export default function Modal(movie, type){
 
+    const {movieId} = useParams();
+    const navigate = useNavigate();
+
+    const mediaType = movie.media_type === 'tv';
 
     return (
         
