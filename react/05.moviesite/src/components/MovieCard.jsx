3d1eb311e6e9294f3061e4c8ee0e14b0d57f52ef
saugjong0521 <saugjong0521@gmail.com>
2024-10-18 20:03:20 +0900

    vault backup: 2024-10-18 20:03:20

--- react/05.moviesite/src/components/MovieCard.jsx
+++ react/05.moviesite/src/components/MovieCard.jsx
@@ -84,8 +84,6 @@ const MovieItem = styled.div`
                 justify-content: center-between;
                 align-items: center;
                 color: white;
-                font-size: 12px;
-                gap: 8px; /* 간격 추가 */
 
                 div{
                     display: flex;
