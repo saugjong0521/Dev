a06f9daf23a2897cc57b4d83fe4a280f524aa715
saugjong0521 <saugjong0521@gmail.com>
2024-10-21 19:28:15 +0900

    vault backup: 2024-10-21 19:28:14

--- react/05.moviesite/src/pages/SearchResult.jsx
+++ react/05.moviesite/src/pages/SearchResult.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { useLocation, useNavigate } from "react-router-dom"
+import instance from '../axios/axios';
 
 
 export default function SearchResult (){
@@ -8,7 +9,7 @@ export default function SearchResult (){
 
     const location = useLocation()
     const keyWord = location.state.keyword
-    const {isLoading, error, data} = useQuery('search')
+    const {isLoading, error, data} = useQuery(['search', keyword], ()=> instance.getSearch(keyword))
     return (
         <>
         </>
