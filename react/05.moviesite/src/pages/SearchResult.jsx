da93435a5e2be2ea7442c465774c7494dc8faf4c
saugjong0521 <saugjong0521@gmail.com>
2024-10-21 19:25:13 +0900

    vault backup: 2024-10-21 19:25:13

--- react/05.moviesite/src/pages/SearchResult.jsx
+++ react/05.moviesite/src/pages/SearchResult.jsx
@@ -1,6 +1,14 @@
+import { useQuery } from "react-query";
+import { useLocation, useNavigate } from "react-router-dom"
 
 
 export default function SearchResult (){
+    const navigate = useNavigate();
+    // const {movieId}
+
+    const location = useLocation()
+    const keyWord = location.state.keyword
+    const {isLoading, error, data} = useQuery('search')
     return (
         <>
         </>
