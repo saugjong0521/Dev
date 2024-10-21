a9dee44ad483adae289c562e11b052fa1e1821c6
saugjong0521 <saugjong0521@gmail.com>
2024-10-21 20:16:17 +0900

    vault backup: 2024-10-21 20:16:17

--- react/05.moviesite/src/components/MovieCard.jsx
+++ react/05.moviesite/src/components/MovieCard.jsx
@@ -45,7 +45,7 @@ export default function MovieCard({ movie, id, hoverId, type, rate, imgVariants,
                     <div className='infoImg'>
                         <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
                     </div>
-                    <div className="infoWrapper" onClick={handleModal(movie.id)}>
+                    <div className="infoWrapper" onClick={() => handleModal(movie.id)}>
                         <div className="btnsWrapper">
                             <div>
                                 <FaPlay />
