d8147e50dabc42ddc5882cea7895a235414e6e91
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 19:45:46 +0900

    vault backup: 2024-10-23 19:45:46

--- Output/shoppingmall/src/App.js
+++ Output/shoppingmall/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
