ce842fc3940cbe112496bf9b33a06aba89790b1a
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 20:17:08 +0900

    vault backup: 2024-10-23 20:17:08

--- Output/shoppingmall/src/pages/Login.jsx
+++ Output/shoppingmall/src/pages/Login.jsx
@@ -1,10 +1,16 @@
+import styled from "styled-components"
 
 
 export default function Login (){
 
     return (
         <>
-        <h1>로그인</h1>
+        <h1>로그인 페이지</h1>
+        <GoogleBtn onClick={handleGoogleLogin}>구글 로그인</GoogleBtn>
         </>
     )
 }
+
+const GoogleBtn = styled.button`
+    
+`
