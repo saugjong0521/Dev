2e0592c7f92a7cc4eff2ebd2173f05a7a92e578e
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 20:20:10 +0900

    vault backup: 2024-10-23 20:20:10

--- Output/shoppingmall/src/App.js
+++ Output/shoppingmall/src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './style/GlobalStyle';
 
 function App() {
   return (
     <>
+    <GlobalStyle/>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Outlet/>
     </>
   );
 }
