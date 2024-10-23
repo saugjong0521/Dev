a95b450ecbd33fdbc6543a2cd2e5f7af4d0c3256
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 21:18:06 +0900

    vault backup: 2024-10-23 21:18:06

--- Output/shoppingmall/src/api/Firebase.jsx
+++ Output/shoppingmall/src/api/Firebase.jsx
@@ -8,6 +8,7 @@ const firebaseConfig = {
     authDomain : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    databaseURL : process.env.REACT_APP_FIREBASE_DB_URL,
 }
 
 /*
