9504ec8e9c7da4d685c103259ba8deec1064ffd9
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 20:12:12 +0900

    vault backup: 2024-10-23 20:12:12

---
+++ Output/shoppingmall/src/api/Firebase.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+
+
+
+const firebaseConfig = {
+    apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+}
+
+/*
+process.env = 환경변수 node.js에 전역객체
+환경변수: 실행중인 프로세스에 사용할 수 있고, 애플리케이션을 구현하는 키값으로 이루어진 변수
+외부에서 값을 받아와서 설정할 수 있게 코드를 직접 하드코딩하지 않고, 설정, 개인정보와 같은 민감한 정보들을 매개변수로 분리해서 관리하는 용도로 사용
+
+process = 현재 node.js에서 프로세스의 전역객체로 실행중인 프로세스에 접근해서 정보를 받아옴
+.env = process에서 사용할 수 있는 모든 환경변수를 포함하는 객체(기본 환경파일)
+
+*/
+
+const app = initializeApp(firebaseConfig);
