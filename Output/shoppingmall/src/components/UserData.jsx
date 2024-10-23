d248f923d93430b594369a580fdd9b7d1a6bb811
saugjong0521 <saugjong0521@gmail.com>
2024-10-23 20:34:05 +0900

    vault backup: 2024-10-23 20:34:05

--- Output/shoppingmall/src/components/UserData.jsx
+++ Output/shoppingmall/src/components/UserData.jsx
@@ -1,10 +1,20 @@
+import styled from "styled-components"
 
 
 
 export default function UserData({user}){
     return(
         <UserInfo>
+            <img src={user.photoURL} alt={user.displayName}/>
             <span>{user.displayName}</span>
         </UserInfo>
     )
 }
+
+const UserInfo = styled.div`
+    display: flex;
+    align-items: center;
+    span{
+        font-size: 16px;
+    }
+`
