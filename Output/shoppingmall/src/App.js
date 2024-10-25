43b84d8299f7cc0bde5ae3fc7572ec29856fd6b6
saugjong0521 <saugjong0521@gmail.com>
2024-10-25 19:45:57 +0900

    vault backup: 2024-10-25 19:45:57

--- Output/shoppingmall/src/App.js
+++ Output/shoppingmall/src/App.js
@@ -4,16 +4,19 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './style/GlobalStyle';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
     <GlobalStyle/>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
       <Outlet/>
+    </AuthContextProvider>
     </>
   );
 }
