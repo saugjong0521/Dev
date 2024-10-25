4c6c276ea80707e7218b11da04eb976c101f1fb0
saugjong0521 <saugjong0521@gmail.com>
2024-10-25 20:24:56 +0900

    vault backup: 2024-10-25 20:24:56

--- Output/shoppingmall/src/pages/AdminSection.jsx
+++ Output/shoppingmall/src/pages/AdminSection.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 
 
 export default function AdminSection(){
+
+    const [file, setFile] = useState(null); // 파일 업로드
+    const [isLoading, setIsLoading] = useState(false);
+    const [success, setSuccess] = useState(null);
+    const [error, setError] = useState(null);
+
     return(
         <>
             <h1>어드민섹션</h1>
