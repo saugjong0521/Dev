93639ddaac261166e588de0d701cf56deb87ffaf
saugjong0521 <saugjong0521@gmail.com>
2024-10-25 21:16:56 +0900

    vault backup: 2024-10-25 21:16:56

--- Output/shoppingmall/src/api/ImgUpload.jsx
+++ Output/shoppingmall/src/api/ImgUpload.jsx
@@ -4,6 +4,20 @@ export async function uploadImg(file){
     try {
         const formData = new FormData();
         formData.append('file', file);
-        formData.append('upload_preset');
+        formData.append('upload_preset', REACT_APP_CLOUDINARY_PRESET);
+
+        const res = await fetch(REACT_APP_CLOUDINARY_URL, {
+
+            method: 'POST',
+            body: formData,
+        });
+        if(!res.ok){
+            console.error(res.state)
+        }
+        const data = await res.json();
+        console.log(data)
+        return data.url;
+    } catch(error){
+        console.error(error)
     }
 }
