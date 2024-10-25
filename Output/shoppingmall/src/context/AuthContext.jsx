5bfb52d5ba29385fb2683b903f51e97e1a29ad0f
saugjong0521 <saugjong0521@gmail.com>
2024-10-25 19:29:57 +0900

    vault backup: 2024-10-25 19:29:57

---
+++ Output/shoppingmall/src/context/AuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { googleLogin, googleLogout, onUserState } from "../api/Firebase";
+
+
+
+const AuthContext = createContext();
+// context: 컴포넌트간에 어떠한 값을 공유할 수 있게 해주는 hook;
+// 변수에 새로운 context를 초기화
+// createContext() = 컨택스트를 사용하기 위해서 생성
+
+
+export function AuthContextProvider ({children}){
+    const [user, setUser] = useState();
+    const [unSubscribe, setUnSubscribe] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        /*
+        페이지를 새로고침하는 경우, 페이지에서 사용자의 정보가 넘어오기 전에 인증을 끝내기 때문에 protextRouter로 인해 에러가 나오는 경우가 생김
+        사용자 정보를 모두 받아오기 전까지 protectRouter를 실행하지 못하게 지연시키는 방법으로 접근
+        */
+
+        const storeUser = sessionStorage.getItem('user');
+        // sessionStorage = 웹 브라우저에서 제공하는 저장소, 페이지가 유지되는 동안 데이터를 저장하는 용도
+        // 현재 열린 창을 닫으면 데이터가 삭제되므로, 임시 데이터를 저장하는데에 사용
+        // 다크모드같은 경우는 세션스토리지가 아닌 로컬스토리지에 저장해서 유지
+        
+        if(storeUser){
+            setUser(JSON.parse(storeUser))
+            // JSON.parse는 JSON.stringify로 저장된 데이터를 자바스크립트 객체로 변환
+        }
+        
+        //로그인, 로그아웃을 확인하는 함수
+        const userChange = (newUser) => {
+            setUser(newUser)
+            setIsLoading(false);
+
+            if(newUser){
+                sessionStorage.setItem('user', JSON.stringify(newUser));
+                // 로그인 시 sessionStorage에 'user' 데이터를 저장
+            } else {
+                sessionStorage.removeItem('user')
+                // 로그아웃 시, 'user' 데이터를 제거
+            }
+            const unSubscribeFunc = onUserState(userChange)
+            setUnSubscribe(()=>unSubscribeFunc);
+            return () => {
+                if(unSubscribeFunc){
+                    unSubscribeFunc();
+                }
+            }
+        }
+    },[]) // useEffect
+
+    return(
+        <AuthContextProvider value={{user,googleLogin,googleLogout, isLoading, uid:user && user.uid}}>
+            {children}
+        </AuthContextProvider>
+    )
+
+}
+
+export function userAuthContet(){
+    return useContext(AuthContext)
+}
+// 다른 곳에서 참조할 수 있도록 context를 export
