023a7cf9bfd2a1c394565d2d52a65112c7144c77
saugjong0521 <saugjong0521@gmail.com>
2024-10-28 21:29:43 +0900

    vault backup: 2024-10-28 21:29:43

--- Output/shoppingmall/src/pages/CategoryPages.jsx
+++ Output/shoppingmall/src/pages/CategoryPages.jsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom"
+import CategoryProductsList from "../components/CategoryProductsList";
+import { useState } from "react";
 
 
 export default function CategoryPages(){
+    const [product, setProduct] = useState([]);
 
     const {category} = useParams();
 
     return (
-        <h1>{category}</h1>
+        <>
+            <h1>{category}</h1>
+            <CategoryProductsList category={category} product={product}/>
+        </>
     )
 
 }
