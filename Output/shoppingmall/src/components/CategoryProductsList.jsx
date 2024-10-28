d027ec9af052fad0a79484323c080f8fb432f8aa
saugjong0521 <saugjong0521@gmail.com>
2024-10-28 21:26:45 +0900

    vault backup: 2024-10-28 21:26:45

--- Output/shoppingmall/src/components/CategoryProductsList.jsx
+++ Output/shoppingmall/src/components/CategoryProductsList.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 
 
 export default function CategoryProductsList ({category, product}){
 
+    const [products, setproducts] = useState(product);
+
     return(
-        <></>
+        <div className="container">
+            <h2>{category}</h2>
+
+            <ul className="productList">
+                {products}
+            </ul>
+        </div>
     )
 
 }
