20a05dd3f4f5b4be6ab4f28b57b0e0c99f1d5414
saugjong0521 <saugjong0521@gmail.com>
2024-10-30 20:12:39 +0900

    vault backup: 2024-10-30 20:12:39

--- Output/shoppingmall/src/context/UseCart.jsx
+++ Output/shoppingmall/src/context/UseCart.jsx
@@ -7,10 +7,14 @@ export default function (){
 
     // useQueryClient = 리액트에서 데이터를 가져오고 업데이트 하는 라이브러리 (yarn add @tanstack/react-query)
 
-    const queryCliect = useQueryClient();
+    const queryClient = useQueryClient();
 
-    // const addItemCart = useMutation({
-    //     mutationFn : (product) = 
-    // });
+    const addItemCart = useMutation({
+        mutationFn : (product) = updateCart(uid, product),
+        onSuccess : ()=>{
+            queryClient.invalidateQueries(['cart', uid])
+            //상태값을 최신으로 갱신(쿠키값을 무효화 시켜서 상품의 정보를 최신으로 업데이트)
+        }
+    });
     // useMutation 장바구니에 상품 추가를 업데이트하는 메소드
 }
