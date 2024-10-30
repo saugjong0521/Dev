69bfa7c59d7fc6ac68ef18007ff99e5bf5c53f11
saugjong0521 <saugjong0521@gmail.com>
2024-10-30 21:18:44 +0900

    vault backup: 2024-10-30 21:18:44

--- Output/shoppingmall/src/context/UseCart.jsx
+++ Output/shoppingmall/src/context/UseCart.jsx
@@ -4,7 +4,7 @@ import { updateCart } from "../api/Firebase";
 
 
 export default function (){
-    const {uid} = useAuthContext();
+    const {user.uid} = useAuthContext();
 
     // useQueryClient = 리액트에서 데이터를 가져오고 업데이트 하는 라이브러리 (yarn add @tanstack/react-query)
 
