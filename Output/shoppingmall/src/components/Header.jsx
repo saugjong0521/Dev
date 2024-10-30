a1c967e7f881bcfa73f86195c13f6eb8e20010a9
saugjong0521 <saugjong0521@gmail.com>
2024-10-30 20:38:43 +0900

    vault backup: 2024-10-30 20:38:43

--- Output/shoppingmall/src/components/Header.jsx
+++ Output/shoppingmall/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header (){
         })
     },[])
 
-    console.log(user)
+    // console.log(user)
 
     const handleLogout = () => {
         googleLogout().then(setUser);
