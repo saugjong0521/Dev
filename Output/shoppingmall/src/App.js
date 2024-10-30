21ffce91c4fafb93c237a6dd93e8e1242e448932
saugjong0521 <saugjong0521@gmail.com>
2024-10-30 20:24:40 +0900

    vault backup: 2024-10-30 20:24:40

--- Output/shoppingmall/src/App.js
+++ Output/shoppingmall/src/App.js
@@ -5,21 +5,16 @@ import Header from './components/Header';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './style/GlobalStyle';
 import { AuthContextProvider } from './context/AuthContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-    <QueryClientProvider client={queryClient}>
     <GlobalStyle/>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
       <Outlet/>
-    </QueryClientProvider>
     </>
   );
 }
