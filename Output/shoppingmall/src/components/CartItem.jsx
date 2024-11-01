2ea679857e62ad6af1e57bf0b4450867875b3522
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 19:35:38 +0900

    vault backup: 2024-11-01 19:35:38

--- Output/shoppingmall/src/components/CartItem.jsx
+++ Output/shoppingmall/src/components/CartItem.jsx
@@ -4,7 +4,14 @@ export default function CartItem ({product, index}){
 
     return(
         <li>
-            {}
+            <p>{index}</p>
+            <img src={product.img} alt={product.title}/>
+            <p>{product.title}</p>
+            <p>{product.size}</p>
+            <p>{product.price}</p>
+            <div className="quantityWrap">
+                <p>수량: {product.quantity}</p>
+            </div>
         </li>
     )
 
