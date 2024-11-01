793254ef01586c7e8eac072716f6b81a55e77462
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 20:44:36 +0900

    vault backup: 2024-11-01 20:44:36

--- Output/shoppingmall/src/pages/CategoryPages.jsx
+++ Output/shoppingmall/src/pages/CategoryPages.jsx
@@ -25,6 +25,12 @@ export default function CategoryPages(){
     useEffect(()=>{
         if(product.length > 0){
             const randomImg = [...product].sort(()=>0.5-Math.random())
+            // 배열을 램덤하게 섞어주는 정렬
+            // sort 메소드는 배열의 요소를 정렬하기 위해 비교함
+            // 이 비교 함수에서는 배열의 두 요소 a,b를 비교해서 양수, 음수, 0을 반환
+            // 음수 (a<b)면 a가 b보다 앞에 위치
+            // 양수 (a>b)면 a가 b보다 뒤에 위치
+            // 0 : 위치를 변경하지 않음
         }
     })
 
