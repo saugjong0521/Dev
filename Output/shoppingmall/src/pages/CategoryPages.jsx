693615d9f8d2429d4a76173006fbb3d76fea2d44
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 20:37:38 +0900

    vault backup: 2024-11-01 20:37:38

--- Output/shoppingmall/src/pages/CategoryPages.jsx
+++ Output/shoppingmall/src/pages/CategoryPages.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import CategoryProductsList from "../components/CategoryProductsList";
 import { useEffect, useState } from "react";
 import { getCategoryProduct } from "../api/Firebase";
+import CategorySlider from "../components/CategorySlider";
 
 
 export default function CategoryPages(){
@@ -21,6 +22,7 @@ export default function CategoryPages(){
 
     return (
         <>
+        <CategorySlider/>
             <CategoryProductsList category={category} product={product}/>
         </>
     )
