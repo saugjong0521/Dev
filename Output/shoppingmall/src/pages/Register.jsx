1667f3b4dc36c2b00daa13c4f17cc6201f36fef6
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 21:29:36 +0900

    vault backup: 2024-11-01 21:29:36

--- Output/shoppingmall/src/pages/Register.jsx
+++ Output/shoppingmall/src/pages/Register.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
 
 
 export default function Register(){
 
+    const [userName, setUserName] = useState('')
+
     return(
         <div className="container">
-            
+            <h2>회원가입</h2>
+            <form>
+                <div>
+                    <input type="email"></input>
+                    <input type="text" placeholder="이름을 입력하세요"/>
+                    
+                </div>
+            </form>
         </div>
     )
 
