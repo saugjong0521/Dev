e34ce01abd2c844a17d3ef2e1dac9fbe75adf007
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 19:22:36 +0900

    vault backup: 2024-11-01 19:22:36

--- Output/shoppingmall/src/context/UseCart.jsx
+++ Output/shoppingmall/src/context/UseCart.jsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAuthContext } from "./AuthContext";
-import { updateCart } from "../api/Firebase";
+import { getCart, updateCart } from "../api/Firebase";
 
 
 export default function (){
@@ -25,7 +25,9 @@ export default function (){
     // 장바구니 아이템의 경우 사용자가 아이템의 목록을 삭제하거나 수정할 수도 있기 때문에, 업데이트 내역을 상시로 갱신해줄 수 있는 useQuery로 받아와야 한다.
     const cartInfo = useQuery({
         // cart의 데이터를 가져오는 비동기 쿼리 설정
-        queryKey : ['cart', uid || '']// 쿼리를 식별해주는 키
+        queryKey : ['cart', uid || ''],// 쿼리를 식별해주는 키
+        queryFn : () => getCart(uid), // 데이터를 가져오는 함수
+        enabled : !!uid // 쿼리를 활성화 되어야 하는 조건(!!)
     })
     
 
