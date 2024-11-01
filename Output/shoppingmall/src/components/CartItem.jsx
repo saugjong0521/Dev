2311c3f2065ae5ddf45283913121085deb859924
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 20:04:35 +0900

    vault backup: 2024-11-01 20:04:35

--- Output/shoppingmall/src/components/CartItem.jsx
+++ Output/shoppingmall/src/components/CartItem.jsx
@@ -9,6 +9,14 @@ export default function CartItem ({product, index}){
         addItemCart.mutate({...product, quantity: product.quantity + 1})
     }
 
+    const handleItemSubstract = ()=> {
+        if(product.quantity < 2){
+            alert('상품 갯수는 1개보다 적을 수 없습니다.');
+            return;
+        }
+        addItemCart.mutate({...product, quantity: product.quantity - 1})
+    }
+
     return(
         <li>
             <p>{index}</p>
@@ -19,7 +27,7 @@ export default function CartItem ({product, index}){
             <div className="quantityWrap">
                 <p>수량: {product.quantity}</p>
                 <button onClick={handleItemAdd}>+</button>
-                <button>-</button>
+                <button onClick={handleItemSubstract}>-</button>
             </div>
         </li>
     )
