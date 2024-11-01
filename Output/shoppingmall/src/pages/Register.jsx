3bc730469286d67a0a3958367143a6ee3298d7cc
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 21:32:38 +0900

    vault backup: 2024-11-01 21:32:37

--- Output/shoppingmall/src/pages/Register.jsx
+++ Output/shoppingmall/src/pages/Register.jsx
@@ -11,12 +11,16 @@ export default function Register(){
     const [emailErr, setEmailErr] = useState('');
     const [pwErr, setPwErr] = useState('');
 
+    //이름 유효형 검사
+    const validateName = (userName)
+
     return(
         <div className="container">
             <h2>회원가입</h2>
             <form>
                 <div>
                     <input type="email"></input>
+                    <input type="password"></input>
                     <input type="text" placeholder="이름을 입력하세요"/>
                     
                 </div>
