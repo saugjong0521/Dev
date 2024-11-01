3199a8e2c948109fe949a55022e3b4507ca59e97
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 19:27:36 +0900

    vault backup: 2024-11-01 19:27:36

--- Output/shoppingmall/src/pages/MyCart.jsx
+++ Output/shoppingmall/src/pages/MyCart.jsx
@@ -1,6 +1,9 @@
+import UseCart from "../context/UseCart"
 
 
 export default function MyCart(){
+
+    const {cartInfo : {data: products}} = UseCart();
     return(
         <>
             <h1>장바구니 페이지</h1>
