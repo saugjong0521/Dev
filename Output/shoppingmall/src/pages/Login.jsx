82990bb68c82ab5162bf6ca2ffe3c628f432d4cd
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 21:24:37 +0900

    vault backup: 2024-11-01 21:24:37

--- Output/shoppingmall/src/pages/Login.jsx
+++ Output/shoppingmall/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { googleLogin } from "../api/Firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 
@@ -26,17 +26,20 @@ export default function Login (){
                     type="email" 
                     placeholder="이메일을 입력하세요"
                     value={email}
-                    onChange={''}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input 
                     type="password"
                     placeholder="비밀번호를 입력하세요"
                     value={password}
-                    onChange={''}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
                 <button type="submit">로그인</button>
+                <GoogleBtn onClick={handleGoogleLogin}>구글 로그인</GoogleBtn>
             </form>
-            <GoogleBtn onClick={handleGoogleLogin}>구글 로그인</GoogleBtn>
+
+            <Link to='/join'>회원가입</Link>
+            
         </div>
     )
 }
