d11e490a488b2727acb6d8e9f15acf9992048817
saugjong0521 <saugjong0521@gmail.com>
2024-11-01 20:33:30 +0900

    vault backup: 2024-11-01 20:33:30

---
+++ Output/shoppingmall/src/components/CategorySlider.jsx
@@ -0,0 +1,17 @@
+import { Swiper } from "swiper/react";
+import 'swiper/css'
+
+
+const slider = {
+    width: '500px',
+    height: '600px',
+}
+
+export default function CategorySlider (){
+
+
+    return (
+        <Swiper></Swiper>
+    )
+
+}
