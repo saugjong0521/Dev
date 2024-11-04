acd318fffb5f0a9adb540d7a3c829bcc5a4fe56b
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 21:24:23 +0900

    vault backup: 2024-11-04 21:24:23

--- Output/shoppingmall/src/components/SortBtn.jsx
+++ Output/shoppingmall/src/components/SortBtn.jsx
@@ -16,3 +16,5 @@ SortBtn.PropTypes = {
     sortName : PropTypes.func.isRequired,
     sortPrice : PropTypes.func.isRequired,
 }
+// proptypes는 타입에 대한 검증
+// func는 함수형 타입이어야 하며, isRequired는 반드시 전달되야 하는 값
