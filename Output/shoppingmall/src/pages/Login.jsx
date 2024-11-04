d8d9ec17f4f9560ab83e59ab73dcac83e6740887
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 20:26:22 +0900

    vault backup: 2024-11-04 20:26:22

--- Output/shoppingmall/src/pages/Login.jsx
+++ Output/shoppingmall/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { googleLogin } from "../api/Firebase";
+import { googleLogin, loginEmail } from "../api/Firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
@@ -18,10 +18,25 @@ export default function Login (){
         }
     }
 
+    const handleLogin = asyne (e) => {
+        e.preventDefault();
+        setErrMsg('')
+        try{
+            const user = await loginEmail(email, password)
+            if (user){
+                navigate('/')
+            } else{
+                setErrMsg('회원정보가 일치하지 않습니다.')
+            }
+        } catch(error){
+            console.error(error)
+        }
+    }
+
     return (
         <div className="container">
             <h1>로그인 페이지</h1>
-            <form onSubmit={hangleLoginEvent}>
+            <form onSubmit={handleLogin}>
                 <input 
                     type="email" 
                     placeholder="이메일을 입력하세요"
@@ -37,6 +52,7 @@ export default function Login (){
                 <button type="submit">로그인</button>
             </form>
             <GoogleBtn onClick={handleGoogleLogin}>구글 로그인</GoogleBtn>
+            {errMsg && <span>{errMsg}</span>}
 
 
             <Link to='/register'>회원가입</Link>
