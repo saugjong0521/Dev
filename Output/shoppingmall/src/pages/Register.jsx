290536850acbe670c6fc10f19669f738f2a85ae4
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 19:45:22 +0900

    vault backup: 2024-11-04 19:45:22

--- Output/shoppingmall/src/pages/Register.jsx
+++ Output/shoppingmall/src/pages/Register.jsx
@@ -58,13 +58,13 @@ export default function Register(){
         }
         try{
             const result = await registerEmail(userEmail, userPassword, userName)
-                if(result.error){
-                    if(result.error === 'auth/email-in-use'){
+                if(result.error === 'auth/email-in-use'){
+                    
                         setEmailErr('사용중인 이메일입니다.')
                     }
+                    
+                else{
                     return
-                } else{
-                    navigate('/login')
                 }
             } catch(error){
                 console.error(error)
