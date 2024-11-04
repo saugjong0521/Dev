c5a8031d868e832396b10120f1f522c70e4f97ba
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 20:42:24 +0900

    vault backup: 2024-11-04 20:42:24

--- Output/shoppingmall/src/pages/Search.jsx
+++ Output/shoppingmall/src/pages/Search.jsx
@@ -4,11 +4,17 @@ import { useState } from "react"
 export default function Search(){
 
     const [query, setQuery] = useState('');
-    const [result, setResult] useState([])
+    const [result, setResult] = useState([]);
 
     return(
         <div className="container">
             <input type="text" value={query}/>
+            <ul className="productList">
+                {result.map((el)=>(
+                    
+
+                ))}
+            </ul>
         </div>
     )
 
