dc152f8c68de0a58d4f0a53f57d716052c288814
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 20:40:22 +0900

    vault backup: 2024-11-04 20:40:22

---
+++ Output/shoppingmall/src/pages/Search.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+
+export default function Search(){
+
+    const [query, setQuery] = useState('');
+    const [result, setResult] useState([])
+
+    return(
+        <div className="container">
+            <input type="text" />
+        </div>
+    )
+
+}
