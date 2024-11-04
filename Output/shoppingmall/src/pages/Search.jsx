f723c921e35ff94f049491201c1a61a92e296516
saugjong0521 <saugjong0521@gmail.com>
2024-11-04 21:09:24 +0900

    vault backup: 2024-11-04 21:09:24

--- Output/shoppingmall/src/pages/Search.jsx
+++ Output/shoppingmall/src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { SearchProducts } from "../api/Firebase";
 
 
 export default function Search(){
@@ -6,12 +7,33 @@ export default function Search(){
     const [query, setQuery] = useState('');
     const [result, setResult] = useState([]);
 
+    useEffect(()=>{
+        const fetchProducts = async () => {
+            if(query.trim() === ''){
+                setResult([])
+            } else{
+                try{
+                    const txt = await SearchProducts(query);
+                    setResult(txt)
+                }catch(error){
+                    console.error(error)
+                }
+            }
+        }
+        fetchProducts()
+    }, [query])
+
+    const handleSearchEvent = (e) => {
+        e.preventDefault();
+        setQuery(e.target.value)
+    }
+
     return(
         <div className="container">
-            <input type="text" value={query}/>
+            <input type="text" value={query} onChange={handleSearchEvent}/>
             <ul className="productList">
                 {result.map((el)=>(
-                    
+                    <li>{el.title}</li>
 
                 ))}
             </ul>
