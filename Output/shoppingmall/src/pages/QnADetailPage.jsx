36b6cf2e303f569be53a6394392ae17447f8d0a3
saugjong0521 <saugjong0521@gmail.com>
2024-11-08 08:56:07 +0900

    vault backup: 2024-11-08 08:56:07

--- Output/shoppingmall/src/pages/QnADetailPage.jsx
+++ Output/shoppingmall/src/pages/QnADetailPage.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { onUserState } from "../api/Firebase";
 
 
 export default function QnADetailPage (){
+
+    const state = useLocation().state;
+    const {id, user, date, title, text} = state;
+    const [loginUser, setLoginUser] = useState();
+
+    useEffect(() => {
+        onUserState((user) => {
+            setLoginUser(user)
+        })
+    })
+
+    return(
+
+        <div className="container">
+            <div className="boardBox">
+                <strong>{title}</strong>
+                <p>{text}</p>
+                <p>{date}</p>
+            </div>
+        </div>
+
+    )
 
 }
