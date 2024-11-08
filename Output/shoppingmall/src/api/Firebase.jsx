c7e8dfc6fb63b48780e5fffcbab3059059f2fc14
saugjong0521 <saugjong0521@gmail.com>
2024-11-08 20:30:12 +0900

    vault backup: 2024-11-08 20:30:12

--- Output/shoppingmall/src/api/Firebase.jsx
+++ Output/shoppingmall/src/api/Firebase.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app" ;
-import { get, getDatabase, ref, remove, set } from "firebase/database" ;
+import { get, getDatabase, ref, remove, set, update } from "firebase/database" ;
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { v4 as uuid } from "uuid"
 
