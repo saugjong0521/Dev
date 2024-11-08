557f83bc79925e9c039bdf2caf00f7100690bd48
saugjong0521 <saugjong0521@gmail.com>
2024-11-08 21:24:07 +0900

    vault backup: 2024-11-08 21:24:07

--- Output/shoppingmall/src/components/BestProducts.jsx
+++ Output/shoppingmall/src/components/BestProducts.jsx
@@ -10,10 +10,27 @@ export default function BestProducts(){
             try {
                 const allProducts = await getAllProduct();
 
-                const likeProducts = await
+                const likeProducts = await Promise.all(
+                    allProducts.map(async(product)=>{
+                    const likes = await getLike(product.id)
+                    return {...product, likes}
+                    })
+                )
                 // getLikes를 사용해서 likeProducts를 사용할 셩우 하나의 아이템이 호출될때까지 다른 아이템의 호출을 중단(상품 수가 많으면 지연시간이 오래걸림)
                 // promise.all을 사용하면 동시 호출이 된다.
+
+
+
+                //좋아요 수 기준으로 정렬해서 상위 상품만 출력
+                const bestProducts = likeProducts
+                    .filter((product) => product.likes > 0)
+                    .sort((a,b)=>b.likes - a.likes); // 좋아요 기준 내림차순
+
+                    setProducts(bestProducts)
+            } catch(error){
+                console.error(error)
             }
         }
-    })
+        fetchProducts()
+    }, [])
 }
