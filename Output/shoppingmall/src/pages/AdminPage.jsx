03fa014168fe55cb3a0eb6680b134383093b8272
saugjong0521 <saugjong0521@gmail.com>
2024-11-11 19:32:42 +0900

    vault backup: 2024-11-11 19:32:41

--- Output/shoppingmall/src/pages/AdminPage.jsx
+++ Output/shoppingmall/src/pages/AdminPage.jsx
@@ -34,6 +34,8 @@ export default function AdminPage (){
     /*
     비동기 처리시 async / await의 사용과 then을 사용할 때의 차이점
     async / await = 비동기 코드를 간단하고 읽기 쉽게 하며, 오류처리에 대한 문법을 강제함으로써 직관적인 오류처리를 가능하게 함
+
+    async 비동기데이터 // await 비동기데이터가 가져와지기 전까지 기다리게 함
     */
 
     return(
