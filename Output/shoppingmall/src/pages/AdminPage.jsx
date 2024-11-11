93002034c6faed0992b39b8ba2b2bb3bc4481ff6
saugjong0521 <saugjong0521@gmail.com>
2024-11-11 19:11:03 +0900

    vault backup: 2024-11-11 19:11:03

--- Output/shoppingmall/src/pages/AdminPage.jsx
+++ Output/shoppingmall/src/pages/AdminPage.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react"
+import { getProducts } from "../api/Firebase";
 
 
 export default function AdminPage (){
 
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        getProducts().then(setProducts)
+        // 전체 상품 가져오기
+    })
+
     return(
         <div className="container">
-            <h1>관리</h1>
+            <h2>관리</h2>
+            <div className="adminList"></div>
         </div>
     )
 
