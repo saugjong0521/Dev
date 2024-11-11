bf85f0b88896134547e56ea5c86b0dde44417249
saugjong0521 <saugjong0521@gmail.com>
2024-11-11 20:02:03 +0900

    vault backup: 2024-11-11 20:02:03

--- Output/shoppingmall/src/pages/ProductEdit.jsx
+++ Output/shoppingmall/src/pages/ProductEdit.jsx
@@ -1,6 +1,10 @@
+import { useParams } from "react-router-dom"
 
 
 export default function ProductEdit(){
+
+    const {id} = useParams()//url에 있는 상품 id받아오기
+
     return(
         <div className="container">
             <h2>상품 수정</h2>
