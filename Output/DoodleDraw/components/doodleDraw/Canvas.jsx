e1a9f5e0836d9407513d3478defec871f4566677
saugjong0521 <saugjong0521@gmail.com>
2024-11-15 20:27:47 +0900

    vault backup: 2024-11-15 20:27:46

--- Output/DoodleDraw/components/doodleDraw/Canvas.jsx
+++ Output/DoodleDraw/components/doodleDraw/Canvas.jsx
@@ -1,22 +1,41 @@
-import { useEffect, useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import {styled} from "styled-components";
 
 export default function Canvas(){
 
     const CanvasRef = useRef(null);
+    const ContextRef = useRef(null);
+
+    const [ctx, setCtx] = useState();
+
 
     // 영역 
     useEffect(()=>{
         const canvas = CanvasRef.current;
-        canvas.width = clientX;
-        canvas.height = clientY;
+        canvas.width = CanvasBox.innerWidth;
+        canvas.height = CanvasBox.innerHeight;
+    })
+
+
+    useEffect(()=>{
+        const canvas = CanvasRef.current;
+        const context = CanvasRef.getcontext("2d")
+        context.strokeStyle = "black"
+        context.lineWidth = 2.5;
+        ContextRef.current = context;
+
+        setCtx(ContextRef.current)
     })
 
 
     return(
-
-        <div className="">
-        <canvas ref={CanvasRef}></canvas>
-        </div>
+        <CanvasBox>
+            <canvas ref={CanvasRef}></canvas>
+        </CanvasBox>
     )
 }
+
+const CanvasBox = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
