4f58bdacc5a9398d9c4a2993d0dde7ccd915a790
saugjong0521 <saugjong0521@gmail.com>
2024-11-18 21:43:40 +0900

    vault backup: 2024-11-18 21:43:39

---
+++ typescript/basic/02.function.ts
@@ -0,0 +1,59 @@
+
+
+// 자바 스크립트 방식
+function addJsVer(num01, num02){
+    return num01 + num02;
+}
+console.log(addJsVer(10, 20))
+// 타입 지정이 없음
+
+// 타입 스크립트 방식
+function addTsVer(num01 : number, num02 : number) : number {
+    return num01 + num02
+}
+console.log(addTsVer(10, 20))
+
+
+// 객체를 병합 (자바 스크립트 방식)
+function mergeJsVer (obj01, obj02){
+    return {...obj01, ...obj02}
+}
+console.log(mergeJsVer({name: 'park'},{age: 40}))
+
+// 타입스크립트에서 객체의 병합은 제네릭 방식( 제네릭 방식: 다양한 객체 타입을 병합할 수 있도록 유연성을 제공하는 메소드 )
+/*
+제네릭 방식에서 사용되는 타입변수 이름들
+T : TYPE
+U : TYPE 외에 들어올수 있는 또 다른 타입
+K : 객체의 KEY TYPE
+V : 객체의 VALUE(값) TYPE
+*/
+function mergeTsVer<T,U>(obj01 : T, obj02 : U) : T & U{
+    return{...obj01, ...obj02}
+}
+console.log(mergeTsVer({name : '김성종'}, {age : 29 }))
+
+
+function items<T>(item : T) : T {
+    return item
+}
+console.log(items('text'))
+console.log(items(12345))
+// any와 제네릭 타입의 차이는 결과값은 같지만, 안정성의 차이(타입검사의 진행여부)에 따라 선택할 수 있다. (unknown과 비슷)
+
+
+function result <T> (item : T) : T {
+    return item
+}
+let result01 = result<string>('hi');
+console.log(result01.toUpperCase())
+
+let result02 = result<number>(1212);
+console.log(result02.toUpperCase()) // 에러
+
+
+function resultAny (item : any) : any {
+    return item;
+}
+let resultAny01 = resultAny(1212);
+console.log(resultAny01.toUpperCase())
