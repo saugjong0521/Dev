0db1e08a09f4937ed32f954c86614933afe3bfb3
saugjong0521 <saugjong0521@gmail.com>
2024-11-24 20:28:39 +0900

    vault backup: 2024-11-24 20:28:39

--- typescript/oop/oop02.js
+++ typescript/oop/oop02.js
@@ -1,3 +1,12 @@
 "use strict";
-// oop방식 tea 만들기 관리 로직
-type;
+// 함수대신 클래스로 변경해서 작성
+class TeaMaker {
+    constructor(waterAmount) {
+        // static: 클래스에서 속성이나 메소드를 정의할때 사용,
+        // 해당 속성이나 메서드가 클래스의 인스턴스가 아닌
+        // 클래스 자체에서 속하도록 하는 키워드
+        this.waterAmount = 0;
+        this.waterAmount = waterAmount;
+    }
+}
+TeaMaker.waterTeaBag = 250;
--- typescript/oop/oop02.ts
+++ typescript/oop/oop02.ts
@@ -1,3 +1,20 @@
 
 // oop방식 tea 만들기 관리 로직
-type
+type TeaCup2 = {
+    teaBag : number,
+    isLemon : boolean
+}
+
+// 함수대신 클래스로 변경해서 작성
+class TeaMaker {
+    static waterTeaBag : number = 250;
+    // static: 클래스에서 속성이나 메소드를 정의할때 사용,
+    // 해당 속성이나 메서드가 클래스의 인스턴스가 아닌
+    // 클래스 자체에서 속하도록 하는 키워드
+
+    waterAmount : number = 0;
+
+    constructor(waterAmount: number){
+        this.waterAmount = waterAmount
+    }
+}
