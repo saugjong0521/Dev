a62c4c79340b683c30ae4f011eef6e40f4ac2d09
saugjong0521 <saugjong0521@gmail.com>
2024-11-25 21:11:11 +0900

    vault backup: 2024-11-25 21:11:11

--- nextjs/basic-next/src/app/product/page.jsx
+++ nextjs/basic-next/src/app/product/page.jsx
@@ -1,5 +1,25 @@
+import Link from "next/link"
+
+
+
+
 export default function ProductPage(){
+
+
+    const products = ['item01', 'item02', 'item03', 'item04']
+
+
     return(
+        <>
         <h1>제품 페이지 입니다.</h1>
+        <ul>
+        {products.map((el, idx)=>{
+            <li key={idx}>
+                <Link href={`/product/${el}`}>{el}</Link>
+            </li>
+        })}
+    </ul>
+
+    </>
     )
 }
