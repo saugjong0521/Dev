551b4639db2301e00a135d6ad2dc09cac5302305
saugjong0521 <saugjong0521@gmail.com>
2024-11-25 21:10:10 +0900

    vault backup: 2024-11-25 21:10:10

--- nextjs/basic-next/src/app/about/Page.jsx
+++ nextjs/basic-next/src/app/about/Page.jsx
@@ -1,8 +1,23 @@
+import Link from "next/link"
 
 export default function AboutPage(){
+
+
+    const products = ['item01', 'item02', 'item03', 'item04']
+
+
     return(
+        <>
         <h1>
             about 페이지 입니다.
         </h1>
+        <ul>
+            {products.map((el, idx)=>{
+                <li key={idx}>
+                    <Link href={`/product/${el}`}>{el}</Link>
+                </li>
+            })}
+        </ul>
+        </>
     )
 }
