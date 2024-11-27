1a62f6a195f30422b3d018040ed7794fa007a775
saugjong0521 <saugjong0521@gmail.com>
2024-11-27 20:13:38 +0900

    vault backup: 2024-11-27 20:13:38

--- nextjs/next-optimize/src/app/layout.js
+++ nextjs/next-optimize/src/app/layout.js
@@ -27,9 +27,9 @@ subsets: 웹 폰트에서 특정 문자만 불러오도록 지정(로딩시간�
 */
 
 // local폰트
-// const sunshiney = localFont({
-//   src: "../public/fonts/Sunshiney-Regular.woff2",
-// });
+const sunshiney = localFont({
+  src: "../public/fonts/Sunshiney-Regular.woff2",
+});
 
 
 export const metadata = {
