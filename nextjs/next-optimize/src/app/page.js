9c6cc742a2d691e38dd41cc847c40f09da168be7
saugjong0521 <saugjong0521@gmail.com>
2024-11-27 20:31:36 +0900

    vault backup: 2024-11-27 20:31:35

--- nextjs/next-optimize/src/app/page.js
+++ nextjs/next-optimize/src/app/page.js
@@ -21,7 +21,9 @@ export default function Home() {
       {/* 로컬 이미지 불러오기 */}
       {/* <Image src={bgImage} alt="bg" width={600} height={400}/> */}
 
-      <Image src='https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%ED%99%94%EC%9D%B4%ED%8A%B8-%EC%BC%80%EC%9D%B4%ED%81%AC%EA%B0%80-%EB%86%93%EC%9D%B8-%ED%85%8C%EC%9D%B4%EB%B8%94-%EC%98%86%EC%97%90-%EC%99%80%EC%9D%B8-%ED%95%9C-%EB%B3%91-aYO2uaBlkoI'/>
+      <Image src='https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%ED%99%94%EC%9D%B4%ED%8A%B8-%EC%BC%80%EC%9D%B4%ED%81%AC%EA%B0%80-%EB%86%93%EC%9D%B8-%ED%85%8C%EC%9D%B4%EB%B8%94-%EC%98%86%EC%97%90-%EC%99%80%EC%9D%B8-%ED%95%9C-%EB%B3%91-aYO2uaBlkoI'
+      width={600} height={400}
+      />
     </>
 
   );
