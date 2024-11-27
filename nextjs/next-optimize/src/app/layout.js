97d5d58a185ce7fa43d7eb7abafefc820de8ece7
saugjong0521 <saugjong0521@gmail.com>
2024-11-27 19:16:37 +0900

    vault backup: 2024-11-27 19:16:36

--- nextjs/next-optimize/src/app/layout.js
+++ nextjs/next-optimize/src/app/layout.js
@@ -34,7 +34,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${jaro.classroom}`}>
+      <body className={`${jaro}`}>
         {children}
       </body>
     </html>
