d05e661ff2cf1250f8a8f3260f7128d16e17d672
saugjong0521 <saugjong0521@gmail.com>
2024-11-27 20:22:38 +0900

    vault backup: 2024-11-27 20:22:38

--- nextjs/next-optimize/src/app/layout.js
+++ nextjs/next-optimize/src/app/layout.js
@@ -2,7 +2,7 @@ import localFont from "next/font/local";
 // 구글폰트에서 폰트 import하기
 import {Jaro} from "next/font/google";
 import "./globals.css";
-import {roboto, Roboto} from "./fonts"
+import {roboto} from "./fonts"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,9 +28,9 @@ subsets: 웹 폰트에서 특정 문자만 불러오도록 지정(로딩시간�
 */
 
 // local폰트
-const sunshiney = localFont({
-  src: "../public/fonts/Sunshiney-Regular.woff2",
-});
+// const sunshiney = localFont({
+//   src: "../public/fonts/Sunshiney-Regular.woff2",
+// });
 
 
 export const metadata = {
