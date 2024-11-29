2b6e3ba25b5df827bc1437a7166f195973427eea
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 19:30:43 +0900

    vault backup: 2024-11-29 19:30:42

--- nextjs/todolist/src/api/todoApi.tsx
+++ nextjs/todolist/src/api/todoApi.tsx
@@ -1,10 +1,18 @@
 // 파이어베이스 db를 활용한 함수 정의 (항목 추가, 수정, 삭제)
 
+import { db } from "@/states/firebase";
 import { Todo } from "@/types/todo";
+import { push, ref } from "firebase/database";
 
 // 항목의 추가
-export const addTodoList = async(todo : Omit) => {
+export const addTodoList = async(todo : Omit<Todo, "id">) => {
     /*
     Omit: 특정 타입에서 일부 속성을 제외한 새로운 타입
+    현재 코드에서는 title과 content만 작성되고 id는 외부에서 자동으로 생성되기 때문에
+    Omit을 활용해서 id를 제외한 title과 content만 생성되도록 하용
  ]  */
+    const todoRef = ref(db, `todos `)
+    await push(todoRef, todo)
 }
+
+
