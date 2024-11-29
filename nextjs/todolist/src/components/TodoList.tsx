64ad69bf5a8aad83d98d985aa8a5032386470797
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 19:35:03 +0900

    vault backup: 2024-11-29 19:35:02

---
+++ nextjs/todolist/src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import { Todo } from "@/types/todo";
+
+
+interface TodoListProps{
+    todos: Todo[] // types폴더에 있는 todo의 리스트 배열을 props로 받아옴
+}
+
+const TodoList:React.FC<TodoListProps> = ({todos}) => {
+    return(
+
+    )
+}
