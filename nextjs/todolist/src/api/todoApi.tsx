ab718673a62c004b60a4197ed98d60c3c516b6ff
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 19:29:54 +0900

    vault backup: 2024-11-29 19:29:53

---
+++ nextjs/todolist/src/api/todoApi.tsx
@@ -0,0 +1,10 @@
+// 파이어베이스 db를 활용한 함수 정의 (항목 추가, 수정, 삭제)
+
+import { Todo } from "@/types/todo";
+
+// 항목의 추가
+export const addTodoList = async(todo : Omit) => {
+    /*
+    Omit: 특정 타입에서 일부 속성을 제외한 새로운 타입
+ ]  */
+}
