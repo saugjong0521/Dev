5d60a2a1f791bbdf73cb4e229243666c53731bd1
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 20:42:53 +0900

    vault backup: 2024-11-29 20:42:53

--- nextjs/todolist/src/app/todos/[id]/page.tsx
+++ nextjs/todolist/src/app/todos/[id]/page.tsx
@@ -21,8 +21,8 @@ const TodoDetailPage = () => {
 
     return (
         <div className="p-4">
-            <h2 className="text-2xl font-bold mb-2">{todo.title}</h2>
-            <p>{todo.content}</p>
+            {todo && <h2 className="text-2xl font-bold mb-2">{todo.title}</h2>}
+            {todo && <p>{todo.content}</p>}
         </div>
     )
 }
