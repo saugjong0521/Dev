dda99e8c9f0fad952c48071d76b8abdf84eeed96
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 20:18:26 +0900

    vault backup: 2024-11-29 20:18:25

--- nextjs/todolist/src/states/firebase.tsx
+++ nextjs/todolist/src/states/firebase.tsx
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCGmTIb1iveCTFU_RQHSnvaph2sXe5YLE0",
-    authDomain: "todolist-3cd95.firebaseapp.com",
-    projectId: "todolist-3cd95",
-    storageBucket: "todolist-3cd95.firebasestorage.app",
-    messagingSenderId: "481970756686",
-    appId: "1:481970756686:web:2849471a530c92266b371f"
-  };
+  apiKey: "AIzaSyCGmTIb1iveCTFU_RQHSnvaph2sXe5YLE0",
+  authDomain: "todolist-3cd95.firebaseapp.com",
+  databaseURL: "https://todolist-3cd95-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todolist-3cd95",
+  storageBucket: "todolist-3cd95.firebasestorage.app",
+  messagingSenderId: "481970756686",
+  appId: "1:481970756686:web:2849471a530c92266b371f"
+};
 
   const app = initializeApp(firebaseConfig)
   const db = getDatabase(app)
