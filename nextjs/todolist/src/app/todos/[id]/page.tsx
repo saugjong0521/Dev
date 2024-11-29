4aaf399f7011c1d78acd645902121e031fe58285
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 20:38:59 +0900

    vault backup: 2024-11-29 20:38:58

--- nextjs/todolist/src/app/todos/[id]/page.tsx
+++ nextjs/todolist/src/app/todos/[id]/page.tsx
@@ -16,4 +16,10 @@ const TodoDetailPage = () => {
             })
         }
     },[id])
+
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-bold mb-2"></h2>
+        </div>
+    )
 }
