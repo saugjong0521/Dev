bc3ffc34c7087f44380e8f71e92333ed6fddb126
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 19:22:04 +0900

    vault backup: 2024-11-29 19:22:04

---
+++ nextjs/todolist/src/states/firebase.tsx
@@ -0,0 +1,19 @@
+// 파이어베이스 config 설정
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCGmTIb1iveCTFU_RQHSnvaph2sXe5YLE0",
+    authDomain: "todolist-3cd95.firebaseapp.com",
+    projectId: "todolist-3cd95",
+    storageBucket: "todolist-3cd95.firebasestorage.app",
+    messagingSenderId: "481970756686",
+    appId: "1:481970756686:web:2849471a530c92266b371f"
+  };
+
+  const app = initializeApp(firebaseConfig)
+  const db = getDatabase(app)
+
+
+  export {db}
