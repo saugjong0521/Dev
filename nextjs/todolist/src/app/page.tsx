3fa5f03cf24d210213632222063b8111d95ac08b
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 19:58:53 +0900

    vault backup: 2024-11-29 19:58:53

--- nextjs/todolist/src/app/page.tsx
+++ nextjs/todolist/src/app/page.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [newTitle, setNewTitle] = useState('');
+  const [nextContent, setNewContent] = useState('');
+
   return (
     <div className="container mx-auto">
-      <h1 className="text-4xl font-bold">Todo List</h1>
+      <h1 className="text-4xl font-bold mb-4">Todo List</h1>
+      <div className="mb-4">
+        <input 
+          type="text" 
+          placeholder="제목을 입력하세요"
+        />
+      </div>
     </div>
   );
 }
