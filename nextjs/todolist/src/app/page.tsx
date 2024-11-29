48763b0e00c4bd976f558b5a868bf3ac9f4b1221
saugjong0521 <saugjong0521@gmail.com>
2024-11-29 20:15:57 +0900

    vault backup: 2024-11-29 20:15:57

--- nextjs/todolist/src/app/page.tsx
+++ nextjs/todolist/src/app/page.tsx
@@ -1,5 +1,17 @@
 "use client"
 
+
+/*
+nextJs는 서버사이드 랜더링 방식을 기본으로 사용하고 있다.
+초기 랜더링시 서버에서 html문서를 미리 생성하고 클라이언트로 전송하는 방식이다.
+
+그러나 일부 컴포넌트에서 상태관리, api, 이벤트를 통해서 상태값이 동적으로 관리되야하는 파일에서는 해당 서버사이드 랜더링 방식이 맞지 않다.
+미리 만들어진 파일을 제공할 수 없기 때문
+
+그래서 컴포넌트 가장 첫줄에 "use client" 코드를 넣어서 해당 컴포넌트의 구동방식을 서버사이드 랜더링이 아닌, 클라이언트 사이드 랜더링 방식으로 변경해야 한다.
+대표적으로 useState, useEffect가 있다.
+*/
+
 import { addTodoList } from "@/api/todoApi";
 import { Todo } from "@/types/todo";
 import { useState } from "react";
@@ -11,7 +23,8 @@ export default function Home() {
   const [newTitle, setNewTitle] = useState('');
   const [newContent, setNewContent] = useState('');
 
-  const handleAddTodo = async () => {
+  const handleAddTodo = async(e) => {
+    e.preventDefault()
     if(newTitle && newContent){
       await addTodoList({title: newTitle, content: newContent})
       setNewContent('');
