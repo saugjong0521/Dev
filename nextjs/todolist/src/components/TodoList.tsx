a41e0de621c447b35592fdde6b5befe11d001fad
saugjong0521 <saugjong0521@gmail.com>
2024-12-04 21:39:35 +0900

    vault backup: 2024-12-04 21:39:35

--- nextjs/todolist/src/components/TodoList.tsx
+++ nextjs/todolist/src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { removeTodoList } from "@/api/todoApi";
 import { Todo } from "@/types/todo";
 import Link from "next/link";
 
@@ -11,12 +12,18 @@ const TodoList:React.FC<TodoListProps> = ({todos}) => {
     React.FC
     함수형 컴포넌트를 생성하는 타입스크립트 타입 유형
     */
+
+    const handleRemoveTodo = async (id: string) => {
+        await removeTodoList(id)
+    }
+
     return(
         <div className="todoList">
             {todos.map((todo)=>(
                 <div key={todo.id} className="p-4 border-b">
                     <Link href={`/todos/${todo.id}`}>{todo.title}</Link>
                     <Link href={`/todos/edit/${todo.id}`}>수정</Link>
+                    <button onClick={()=>handleRemoveTodo(todo.id)}>삭제하기</button>
                 </div>
             ))}
         </div>
