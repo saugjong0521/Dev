dc84ae1875287d21fd9e5d6fdd3766956b26f996
saugjong0521 <saugjong0521@gmail.com>
2024-12-04 20:24:33 +0900

    vault backup: 2024-12-04 20:24:32

--- nextjs/todolist/src/app/todos/edit/[id]/page.tsx
+++ nextjs/todolist/src/app/todos/edit/[id]/page.tsx
@@ -1,6 +1,7 @@
+import { getTodoList } from "@/api/todoApi";
 import { Todo } from "@/types/todo";
 import { useParams } from "next/navigation"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const EditTodoPage = () => {
@@ -8,4 +9,19 @@ const EditTodoPage = () => {
     const [todo, setTodo] = useState<Todo | null>(null);
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+
+    useEffect(() => {
+        getTodoList().then((todos)=>{
+            const findTodo = todos.find((el) => el.id ===id)
+            if(findTodo){
+                setTodo(findTodo);
+                setTitle(findTodo.title);
+                setContent(findTodo.content)
+            }
+        })
+    },[id])
+
 }
+
+
+export default EditTodoPage
