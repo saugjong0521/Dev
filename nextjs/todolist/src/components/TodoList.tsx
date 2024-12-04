690672c27f63379b2cd2b69de94652caaf9b4875
saugjong0521 <saugjong0521@gmail.com>
2024-12-04 21:45:35 +0900

    vault backup: 2024-12-04 21:45:35

--- nextjs/todolist/src/components/TodoList.tsx
+++ nextjs/todolist/src/components/TodoList.tsx
@@ -7,7 +7,7 @@ interface TodoListProps{
     todos: Todo[] // types폴더에 있는 todo의 리스트 배열을 props로 받아옴
 }
 
-const TodoList:React.FC<TodoListProps> = ({todos}) => {
+const TodoList:React.FC<TodoListProps> = ({todos, reloadTodoList}) => {
     /*
     React.FC
     함수형 컴포넌트를 생성하는 타입스크립트 타입 유형
@@ -15,6 +15,7 @@ const TodoList:React.FC<TodoListProps> = ({todos}) => {
 
     const handleRemoveTodo = async (id: string) => {
         await removeTodoList(id)
+        reloadTodoList()
     }
 
     return(
