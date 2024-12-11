d159a51d7b8f6a833e1af243acccb014e10738bf
saugjong0521 <saugjong0521@gmail.com>
2024-12-11 20:42:29 +0900

    vault backup: 2024-12-11 20:42:29

--- nextjs/chat/src/components/socketProvider.tsx
+++ nextjs/chat/src/components/socketProvider.tsx
@@ -13,5 +13,18 @@ export type NextApiRes = NextApiResponse & {
 }
 
 const ioHandler = (req: NextApiRequest, res: NextApiRes) => {
-    if(!res.socket.server.io)
+    if(!res.socket.server.io){
+        // 서버가 생성되지 않은 경우, io를 초기화해서 생성
+        const path =  "" // 서버 설정 경로
+        const httpServer : NetServer = res.socket.server as any;
+        const id = new ServerIO(httpServer, {
+            path: path,
+            addTrailingSlash: false
+            // url끝에 slash 추가하지 않음
+        })
+
+        res.socket.server.io = io // 서버에 인스턴스 추가
+    }
 }
+
+export default ioHandler
