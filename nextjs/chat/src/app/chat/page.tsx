2eef4580edfc1f9fc784c9125364cbe0e4a677d1
saugjong0521 <saugjong0521@gmail.com>
2024-12-11 21:31:15 +0900

    vault backup: 2024-12-11 21:31:15

---
+++ nextjs/chat/src/app/chat/page.tsx
@@ -0,0 +1,33 @@
+import { useSocket } from "@/components/socketProvider";
+import { useEffect, useState } from "react";
+
+
+interface message {
+    userId : number,
+    content : string,
+}
+
+
+
+const ChatPage = () => {
+    const [message, setMessage] = useState<message[]>([]);  // 메시지 상태
+    const [currentMessage, setCurrentMessage] = useState('');   // 현재 작성중인 메시지
+    const [userId, setUserId] = useState(+new Date());  // 타임스탬프로 현재 유저 아이디 생성
+    const {socket, isConnected} = useSocket();
+
+    useEffect(()=>{
+        if(!socket){
+            return
+        }
+
+        // 서버에서 메시지를 수신하면 배열을 업데이트
+        socket.on('message', (data:any) => {
+            setMessage((message)=>[...message, data]);
+        })
+
+        return () => {
+            socket.off('message')
+        }
+    },[socket, message])
+
+}
