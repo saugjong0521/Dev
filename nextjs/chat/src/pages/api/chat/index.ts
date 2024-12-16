eabccf5580af7a2e72b03224c7d7a37c307de1e9
saugjong0521 <saugjong0521@gmail.com>
2024-12-16 19:03:57 +0900

    vault backup: 2024-12-16 19:03:57

--- nextjs/chat/src/pages/api/chat/index.ts
+++ nextjs/chat/src/pages/api/chat/index.ts
@@ -6,7 +6,7 @@ import { NextApiRequest } from "next";
 export default async function handler (req: NextApiRequest, res: any){
     if(req.method === 'POST'){
         const message = req.body // 클라이언트에서 보낸 데이터(메시지)
-        res?.socket?.server?.io?.emit('메시지', message);   // 메시지를 모든 클라이언트에게 전송
+        res?.socket?.server?.io?.emit('message', message);   // 메시지를 모든 클라이언트에게 전송
         res.status(200).json(message);
     }
 }
